20613a89ac9bb906e81948eec9f9f0269d2fa153
Filip Luchianov <filip.luchianov@geminicad.com>
2018-06-15 17:12:18 +0300

    add cars 111 and 121

---
+++ AddCars/cars/car111.py
@@ -0,0 +1,27 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "vw golf 1.9 tdi ,Garantie,Financiering, 98000 km !!!!"
+var_brand = "Volkswagen"
+var_model = "Golf"
+var_desc = "1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "4850"
+var_picspath = "car111"
+var_carroserie = "Berline 3"
+var_month = "feb"
+var_year = "2005"
+var_gas = "Diesel"
+var_transmissie = "Manu"
+var_km = "98000"
+var_doors = "5"
+var_pk = "90"
+var_cilinder = "1896"
+var_co2 = "140"
+var_euro = "euro 4"
+var_options = "ABS, Airbag, Airco, Alarm, Automatische airco, Boordcomputer, Centrale vergrendeling, Elektrische buitenspiegels, Elektrische ramen voor, Getint glas, Metallic lak, Radio & CD, Stuurbekrachtiging, Traction control"
+var_carcolor = "Zwart"
+var_interiorcolor = "Grijs"

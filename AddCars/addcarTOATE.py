50a8f84083f986db5d2a0a6a1a773491ac94e2ee
Filip Luchianov <filip.luchianov@geminicad.com>
2018-06-25 11:37:34 +0300

    remove car108

--- AddCars/addcarTOATE.py
+++ AddCars/addcarTOATE.py
@@ -21,7 +21,7 @@ from AddCars.cars import car104
 from AddCars.cars import car105
 from AddCars.cars import car106
 from AddCars.cars import car107
-from AddCars.cars import car108
+#from AddCars.cars import car108
 from AddCars.cars import car109
 from AddCars.cars import car110
 #from AddCars.cars import car111
@@ -105,9 +105,11 @@ class AdaugaMasini(unittest.TestCase):
     def test_car107(self):
         GeneralFunctions.deleteAddCarFunction(self, car107)
     
-    #@unittest.skip("skip car")    
-    def test_car108(self):
-        GeneralFunctions.deleteAddCarFunction(self, car108)
+    #===========================================================================
+    # #@unittest.skip("skip car")    
+    # def test_car108(self):
+    #     GeneralFunctions.deleteAddCarFunction(self, car108)
+    #===========================================================================
     
     #@unittest.skip("skip car")
     def test_car109(self):

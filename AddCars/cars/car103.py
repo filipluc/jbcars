f9b0299616e4a85bedf461d3c059163d598aef9f
Filip Luchianov <filip.luchianov@geminicad.com>
2018-06-25 13:03:36 +0300

    add car103

---
+++ AddCars/cars/car103.py
@@ -0,0 +1,27 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "peugeot 206/1.4 benzine,garantie,financiering,/72000km/euro4"
+var_brand = "Peugeot"
+var_model = "206"
+var_desc = "1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"	
+var_price = "3700"
+var_picspath = "car103"
+var_carroserie = "Berline 3"
+var_month = "maa"
+var_year = "2006"
+var_gas = "Benzine"
+var_transmissie = "Manu"
+var_km = "72000"
+var_doors = "3"
+var_pk = "75"
+var_cilinder = "1360"
+var_co2 = "152"
+var_euro = "euro 4"
+var_options = "ABS, Airbag, Airco, Radio & CD, Stuurbekrachtiging"
+var_carcolor = "Rood"
+var_interiorcolor = "Grijs"

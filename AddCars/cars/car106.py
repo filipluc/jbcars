dd791d1deed228afea76fb54fbf23cd96055359c
Filip Luchianov <filip.luchianov@geminicad.com>
2018-06-30 16:10:32 +0300

    add cars

---
+++ AddCars/cars/car106.py
@@ -0,0 +1,27 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "mitsubishi pajero 2.5 tdi, 7 zetels"
+var_brand = "Mitsubishi"
+var_model = "Pajero"
+var_desc = "export of handelaar\nCAR PASS\ngekeurd met roos formulier=+250 euro\ntrekhaak=3300 kg"
+var_price = "500"
+var_picspath = "car106"
+var_carroserie = "SUV"
+var_month = "okt"
+var_year = "2002"
+var_gas = "Diesel"
+var_transmissie = "Manu"
+var_km = "197000"
+var_doors = "5"
+var_pk = "115"
+var_cilinder = "2477"
+var_co2 = "285"
+var_euro = "euro 3"
+var_options = "4x4, ABS, Airbag, Airco, Alarm, Automatische airco, Boordcomputer, Centrale vergrendeling, Elektrische buitenspiegels, Elektrische ramen voor, Getint glas, Lichtmetalen velgen, Metallic lak, Mistlampen, Radio & CD, Trekhaak"
+var_carcolor = "Blauw"
+var_interiorcolor = "Grijs"

95868048c30e89fe127faeb43d20a95576de103f
Filip Luchianov <filip.luchianov@geminicad.com>
2018-07-03 09:21:42 +0300

    add car108

---
+++ AddCars/cars/car108.py
@@ -0,0 +1,27 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "citroen c5,1.6 hdi/Garantie,financiering, bwj. 2010/92000 km"
+var_brand = "Citroen"
+var_model = "C5"
+var_desc = "1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "5900"
+var_picspath = "car108"
+var_carroserie = "Berline 2"
+var_month = "jun"
+var_year = "2010"
+var_gas = "Diesel"
+var_transmissie = "Manu"
+var_km = "92000"
+var_doors = "4"
+var_pk = "110"
+var_cilinder = "1560"
+var_co2 = "140"
+var_euro = "euro 4"
+var_options = "ABS, Airbag, Airco, Alarm, Automatische airco, Boordcomputer, Centrale vergrendeling, Cruise control, Elektrische buitenspiegels, Elektrische ramen v + a, Getint glas, Metallic lak, Mistlampen, Navigatiesysteem, Parkeersensor, Radio & CD, Stuurbekrachtiging, Traction control, Verwarmde buitenspiegels"
+var_carcolor = "Grijs"
+var_interiorcolor = "Grijs"

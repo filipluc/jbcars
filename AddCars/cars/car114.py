c86be56f555fccc5d2471711ed8bd8a2d61dab56
Filip Luchianov <filip.luchianov@geminicad.com>
2018-07-05 15:14:20 +0300

    add car114

---
+++ AddCars/cars/car114.py
@@ -0,0 +1,27 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "mercedes E200cdi,     AUTOMAAT,    leder"
+var_brand = "Mercedes"
+var_model = "E Klasse"
+var_desc = "export of handelaar\nCAR PASS\gekeurd met roos formulier=+200 euro\ntrekhaak=1900 kg\n\n"
+var_price = "4000"
+var_picspath = "car114"
+var_carroserie = "Berline 2"
+var_month = "mei"
+var_year = "2004"
+var_gas = "Diesel"
+var_transmissie = "Auto"
+var_km = "222000"
+var_doors = "2"
+var_pk = "122"
+var_cilinder = "2148"
+var_co2 = "168"
+var_euro = "euro 3"
+var_options = "ABS, Airbag, Airco, Alarm, Automatische airco, Boordcomputer, Centrale vergrendeling, Cruise control, Elektrische buitenspiegels, Elektrische ramen v + a, Elektrische zetelverstelling, Getint glas, Lederen bekleding, Lichtmetalen velgen, Metallic lak, Mistlampen, Navigatiesysteem, Parkeersensor, Radio & CD, Stuurbekrachtiging, Traction control, Trekhaak, Verwarmde buitenspiegels"
+var_carcolor = "Goud"
+var_interiorcolor = "Zwart"

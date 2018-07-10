fede2ad90ce65c0abbf388ff6eb19c22d7adc488
Filip Luchianov <filip.luchianov@geminicad.com>
2018-07-10 10:39:11 +0300

    add car127

--- AddCars/addcarTOATE.py
+++ AddCars/addcarTOATE.py
@@ -40,6 +40,7 @@ from AddCars.cars import car123
 from AddCars.cars import car124
 from AddCars.cars import car125
 from AddCars.cars import car126
+from AddCars.cars import car127
 
 
 
@@ -180,6 +181,10 @@ class AdaugaMasini(unittest.TestCase):
     #@unittest.skip("skip car") 
     def test_car126(self):
         GeneralFunctions.deleteAddCarFunction(self, car126)
+        
+    #@unittest.skip("skip car") 
+    def test_car127(self):
+        GeneralFunctions.deleteAddCarFunction(self, car127)
  
     
         
---
+++ AddCars/cars/car127.py
@@ -0,0 +1,27 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "peugeot 807, 2.0 hdi/7 zetels/bwj.2012/euro5"
+var_brand = "Peugeot"
+var_model = "807"
+var_desc = "export of handelaar\ndistributieriem+vliegwiel juist vervangen\n"
+var_price = "4400"
+var_picspath = "car127"
+var_carroserie = "Monovolume"
+var_month = "jul"
+var_year = "2012"
+var_gas = "Diesel"
+var_transmissie = "Manu"
+var_km = "224000"
+var_doors = "5"
+var_pk = "136"
+var_cilinder = "1997"
+var_co2 = "155"
+var_euro = "euro 5"
+var_options = "ABS, Adaptieve lichten, Airbag, Airco, Alarm, Automatische airco, Boordcomputer, Centrale vergrendeling, Cruise control, Elektrische buitenspiegels, Elektrische ramen v + a, Getint glas, Lichtmetalen velgen, Metallic lak, Mistlampen, Parkeersensor, Radio & CD, Stuurbekrachtiging, Traction control"
+var_carcolor = "Grijs"
+var_interiorcolor = "Grijs"

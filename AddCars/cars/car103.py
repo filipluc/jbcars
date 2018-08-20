bdc48ee8eb2308ffa848b1d80ec45abda6456b4a
CORP\Filip.LUCHIANOV <filip.luchianov@geminicad.com>
2018-08-20 17:51:17 +0300

    add car103

---
+++ AddCars/cars/car103.py
@@ -0,0 +1,27 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "ford c max 1.6 hdi, airco, export"
+var_brand = "Ford"
+var_model = "C-Max"
+var_desc = "export of handelaar\n"	
+var_price = "1500"
+var_picspath = "car103"
+var_carroserie = "Monovolume"
+var_month = "jul"
+var_year = "2004"
+var_gas = "Diesel"
+var_transmissie = "Manu"
+var_km = "238000"
+var_doors = "5"
+var_pk = "110"
+var_cilinder = "1560"
+var_co2 = "129"
+var_euro = "euro 3"
+var_options = "ABS, Airbag, Airco, Alarm, Boordcomputer, Centrale vergrendeling, Elektrische ramen voor, Metallic lak, Radio & CD, Stuurbekrachtiging, Traction control, Trekhaak"
+var_carcolor = "Zilver"
+var_interiorcolor = "Grijs"

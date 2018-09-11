a33ad74ae03ef4db15b56eda3b218113f620c7af
Filip LUCHIANOV <Filip.LUCHIANOV@geminicad.com>
2018-09-11 18:21:41 +0300

    add car111

---
+++ AddCars/cars/car111.py
@@ -0,0 +1,27 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "mitsubishi ASX,1.7 d,Garantie,Financiering,/50.000 km/euro 5"
+var_brand = "Mitsubishi"
+var_model = "ASX"
+var_desc = "1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "11500"
+var_picspath = "car111"
+var_carroserie = "SUV"
+var_month = "aug"
+var_year = "2010"
+var_gas = "Diesel"
+var_transmissie = "Manu"
+var_km = "50000"
+var_doors = "5"
+var_pk = "115"
+var_cilinder = "1798"
+var_co2 = "145"
+var_euro = "euro 5"
+var_options = "ABS, Airbag, Airco, Alarm, Bluetooth, Boordcomputer, Camera, Centrale vergrendeling, Cruise control, Elektrische buitenspiegels, Elektrische ramen v + a, Getint glas, Lichtmetalen velgen, Metallic lak, Mistlampen, Navigatiesysteem, Panoramisch dak, Parkeersensor, Radio & CD, Stuurbekrachtiging, Traction control, Verwarmde buitenspiegels, Xenon verlichting"
+var_carcolor = "Grijs"
+var_interiorcolor = "Grijs"

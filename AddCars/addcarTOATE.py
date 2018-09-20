d852d94166b24dc96530ca9582520421b1bd7c0a
CORP\Filip.LUCHIANOV <filip.luchianov@geminicad.com>
2018-09-20 15:08:10 +0300

    add car 108

--- AddCars/addcarTOATE.py
+++ AddCars/addcarTOATE.py
@@ -15,7 +15,7 @@ from selenium.common.exceptions import TimeoutException
 from xmlrunner import *
 from AddCars.general import GeneralFunctions
 from AddCars.cars import car101
-#from AddCars.cars import car102
+from AddCars.cars import car102
 from AddCars.cars import car103
 from AddCars.cars import car104
 from AddCars.cars import car105
@@ -82,11 +82,9 @@ class AdaugaMasini(unittest.TestCase):
     def test_car101(self):
         GeneralFunctions.deleteAddCarFunction(self, car101)
     
-    #===========================================================================
-    # #@unittest.skip("skip car") 
-    # def test_car102(self):
-    #     GeneralFunctions.deleteAddCarFunction(self, car102)
-    #===========================================================================
+    #@unittest.skip("skip car") 
+    def test_car102(self):
+        GeneralFunctions.deleteAddCarFunction(self, car102)
     
     #@unittest.skip("skip car")   
     def test_car103(self):

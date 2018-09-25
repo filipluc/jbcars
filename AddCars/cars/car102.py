035eff987e841f8abb933fc002dc21dd4815dfb5
CORP\Filip.LUCHIANOV <filip.luchianov@geminicad.com>
2018-09-25 15:20:13 +0300

    add car102

---
+++ AddCars/cars/car102.py
@@ -0,0 +1,27 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "opel zafira 2.0 cdti, OPC line ,Garantie,bwj. 2014/40.000 km"
+var_brand = "Opel"
+var_model = "Zafira"
+var_desc = "1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "15900"
+var_picspath = "car102"
+var_carroserie = "Monovolume"
+var_month = "jan"
+var_year = "2014"
+var_gas = "Diesel"
+var_transmissie = "Manu"
+var_km = "40000"
+var_doors = "5"
+var_pk = "165"
+var_cilinder = "1956"
+var_co2 = "139"
+var_euro = "euro 5"
+var_options = "ABS, Adaptieve lichten, Airbag, Airco, Alarm, Automatische airco, Bluetooth, Boordcomputer, Camera, Centrale vergrendeling, Cruise control, Elektrische buitenspiegels, Elektrische ramen v + a, Getint glas, Lederen bekleding, Lichtmetalen velgen, Metallic lak, Mistlampen, Navigatiesysteem, Parkeersensor, Radio & CD, Stuurbekrachtiging, Traction control, Verwarmde buitenspiegels, Xenon verlichting, Zetelverwarming"
+var_carcolor = "Wit"
+var_interiorcolor = "Zwart"

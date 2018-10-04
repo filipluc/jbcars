7c63f60cbb2aa86571a4567b4750d8baeb172c8e
CORP\Filip.LUCHIANOV <filip.luchianov@geminicad.com>
2018-10-04 17:14:22 +0300

    add car118

---
+++ AddCars/cars/car118.py
@@ -0,0 +1,27 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "fiat ulysse 2.0 jtd,Garantie,Financiering,109000 km/7 zetels"
+var_brand = "Fiat"
+var_model = "Ulysse"
+var_desc = "1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "3900"
+var_picspath = "car118"
+var_carroserie = "Monovolume"
+var_month = "jul"
+var_year = "2001"
+var_gas = "Diesel"
+var_transmissie = "Manu"
+var_km = "109000"
+var_doors = "5"
+var_pk = "110"
+var_cilinder = "1997"
+var_co2 = ""
+var_euro = "euro 3"
+var_options = "ABS, Airbag, Airco, Alarm, Automatische airco, Boordcomputer, Centrale vergrendeling, Elektrische buitenspiegels, Elektrische ramen v + a, Getint glas, Metallic lak, Radio & CD, Stuurbekrachtiging"
+var_carcolor = "Zwart"
+var_interiorcolor = "Grijs"

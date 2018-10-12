6cd7a9ab1d1d4d2e4e335d16a1068cdbd3e921ea
Filip Luchianov <filip.luchianov@gmail.com>
2018-10-12 21:15:28 +0300

    122

---
+++ AddCars/cars/car122.py
@@ -0,0 +1,27 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "renault scenic 1.5 dci,Garantie,Financiering,euro 4"
+var_brand = "Renault"
+var_model = "Scenic"
+var_desc = "1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "2500"
+var_picspath = "car122"
+var_carroserie = "Monovolume"
+var_month = "jan"
+var_year = "2006"
+var_gas = "Diesel"
+var_transmissie = "Manu"
+var_km = "221000"
+var_doors = "5"
+var_pk = "88"
+var_cilinder = "1461"
+var_co2 = "137"
+var_euro = "euro 4"
+var_options = "ABS, Airbag, Airco, Alarm, Boordcomputer, Centrale vergrendeling, Elektrische buitenspiegels, Elektrische ramen v + a, Getint glas, Lichtmetalen velgen, Metallic lak, Radio & CD, Stuurbekrachtiging, Traction control"
+var_carcolor = "Zilver"
+var_interiorcolor = "Grijs"

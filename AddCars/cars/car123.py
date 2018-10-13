61b8a9359a6e0776574d2348025a041cf1ada8f3
Filip Luchianov <filip.luchianov@gmail.com>
2018-10-13 14:47:30 +0300

    123

---
+++ AddCars/cars/car123.py
@@ -0,0 +1,27 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "peugeot partner 1.4 benzine,Garantie,financiering,5 zit"
+var_brand = "Peugeot"
+var_model = "Partner"
+var_desc = "1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "4900"
+var_picspath = "car123"
+var_carroserie = "Monovolume"
+var_month = "jan"
+var_year = "2008"
+var_gas = "Benzine"
+var_transmissie = "Manu"
+var_km = "117000"
+var_doors = "5"
+var_pk = "75"
+var_cilinder = "1360"
+var_co2 = "175"
+var_euro = "euro 4"
+var_options = "Airbag, Alarm, Centrale vergrendeling, Elektrische buitenspiegels, Elektrische ramen voor, Metallic lak, Radio & CD, Stuurbekrachtiging"
+var_carcolor = "Rood"
+var_interiorcolor = "Grijs"

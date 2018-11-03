6494010d913ae39b75a9bbd862bbb00d75c36685
Filip Luchianov <filip.luchianov@gmail.com>
2018-11-03 14:59:01 +0200

    108

---
+++ AddCars/cars/car108.py
@@ -0,0 +1,27 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "bmw 520 benzine,Garantie,Financiering,AUTOMAAT"
+var_brand = "BMW"
+var_model = "5 reeks"
+var_desc = "1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "6900"
+var_picspath = "car108"
+var_carroserie = "Berline 2"
+var_month = "nov"
+var_year = "2003"
+var_gas = "Benzine"
+var_transmissie = "Auto"
+var_km = "100000"
+var_doors = "4"
+var_pk = "170"
+var_cilinder = "2171"
+var_co2 = ""
+var_euro = "euro 4"
+var_options = "ABS, Airbag, Airco, Alarm, Boordcomputer, Centrale vergrendeling, Cruise control, Elektrische buitenspiegels, Elektrische ramen voor, Elektrische zetelverstelling, Lederen bekleding, Lichtmetalen velgen, Metallic lak, Mistlampen, Navigatiesysteem, Radio & CD, Stuurbekrachtiging, Traction control, Verwarmde buitenspiegels"
+var_carcolor = "Blauw"
+var_interiorcolor = "Grijs"

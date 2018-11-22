7fdc8f119307f2770b3e06b567b3b78a8831ec71
Filip Luchianov <filip.luchianov@gmail.com>
2018-11-22 17:38:42 +0200

    105

---
+++ AddCars/cars/car105.py
@@ -0,0 +1,27 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "peugeot partner 1.6 hdi,Lichte vracht,bwj 2009,CAR PASS"
+var_brand = "Peugeot"
+var_model = "Partner"
+var_desc = "4131 € + btw/tva\nnieuwe koppeling\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "4999"
+var_picspath = "car105"
+var_carroserie = "Berline 3"
+var_month = "nov"
+var_year = "2009"
+var_gas = "Diesel"
+var_transmissie = "Manu"
+var_km = "150000"
+var_doors = "4"
+var_pk = "75"
+var_cilinder = "1560"
+var_co2 = ""
+var_euro = "euro 4"
+var_options = "ABS, Airbag, Boordcomputer, Centrale vergrendeling, Elektrische ramen voor, Metallic lak, Radio & CD, Stuurbekrachtiging, Traction control, Trekhaak"
+var_carcolor = "Zwart"
+var_interiorcolor = "Grijs"

33f7fe6eecd635e7701496bfd8b261b12f3df926
Filip Luchianov <filip.luchianov@gmail.com>
2018-11-29 12:31:41 +0200

    108

---
+++ AddCars/cars/car108.py
@@ -0,0 +1,28 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "peugeot 308, 1.6 benzine,Garantie,bwj.2011/ 24.000 km,euro 5"
+var_categorie = "Auto's"
+var_brand = "Peugeot"
+var_model = "308"
+var_desc = "1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "7999"
+var_picspath = "car108"
+var_carroserie = "Berline 3"
+var_month = "feb"
+var_year = "2011"
+var_gas = "Benzine"
+var_transmissie = "Manu"
+var_km = "24000"
+var_doors = "5"
+var_pk = "120"
+var_cilinder = "1598"
+var_co2 = "147"
+var_euro = "euro 5"
+var_options = "ABS, Airbag, Airco, Alarm, Automatische airco, Boordcomputer, Centrale vergrendeling, Cruise control, Elektrische buitenspiegels, Elektrische ramen v + a, Lichtmetalen velgen, Metallic lak, Mistlampen, Parkeersensor, Radio & CD, Stuurbekrachtiging, Traction control"
+var_carcolor = "Grijs"
+var_interiorcolor = "Grijs"

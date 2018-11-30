a8ab0e01227d873a62e1ea2a26ce2cae82552073
Filip Luchianov <filip.luchianov@gmail.com>
2018-11-30 16:51:37 +0200

    113

--- AddCars/addcarTOATE.py
+++ AddCars/addcarTOATE.py
@@ -26,7 +26,7 @@ from AddCars.cars import car109
 from AddCars.cars import car110
 from AddCars.cars import car111
 from AddCars.cars import car112
-# from AddCars.cars import car113
+from AddCars.cars import car113
 from AddCars.cars import car114
 from AddCars.cars import car115
 from AddCars.cars import car116
@@ -114,8 +114,8 @@ class AdaugaMasini(unittest.TestCase):
     def test_car112(self):
         GeneralFunctions.deleteAddCarFunction(self, car112)
         
-    # def test_car113(self):
-    #     GeneralFunctions.deleteAddCarFunction(self, car113)
+    def test_car113(self):
+        GeneralFunctions.deleteAddCarFunction(self, car113)
     
     def test_car114(self):
         GeneralFunctions.deleteAddCarFunction(self, car114)
---
+++ AddCars/cars/car113.py
@@ -0,0 +1,28 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "peugeot 407 , 1.6 hdi,Garantie,Financiering,bwj.2008/81000km"
+var_categorie = "Auto's"
+var_brand = "Peugeot"
+var_model = "407"
+var_desc = "1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "4999"
+var_picspath = "car113"
+var_carroserie = "Berline 2"
+var_month = "dec"
+var_year = "2008"
+var_gas = "Diesel"
+var_transmissie = "Manu"
+var_km = "80000"
+var_doors = "4"
+var_pk = "110"
+var_cilinder = "1560"
+var_co2 = "140"
+var_euro = "euro 4"
+var_options = "ABS, Airbag, Airco, Alarm, Automatische airco, Boordcomputer, Centrale vergrendeling, Cruise control, Elektrische buitenspiegels, Elektrische ramen v + a, Getint glas, Lichtmetalen velgen, Metallic lak, Mistlampen, Navigatiesysteem, Parkeersensor, Radio & CD, Stuurbekrachtiging, Traction control, Trekhaak"
+var_carcolor = "Grijs"
+var_interiorcolor = "Grijs"

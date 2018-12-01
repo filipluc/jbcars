b0975cc25084e5a5092734ec397dfda65eb50a8c
Filip Luchianov <filip.luchianov@gmail.com>
2018-12-01 15:04:12 +0200

    121

---
+++ AddCars/cars/car121.py
@@ -0,0 +1,28 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "suzuki grand vitara 2.0 d, LICHTE VRACHT,car pass,118000 km"
+var_categorie = "Auto's"
+var_brand = "Suzuki"
+var_model = "Grand Vitara"
+var_desc = "CAR PASS\ngekeurd met roos formulier\n"
+var_price = "3900"
+var_picspath = "car121"
+var_carroserie = "SUV"
+var_month = "jul"
+var_year = "2003"
+var_gas = "Diesel"
+var_transmissie = "Manu"
+var_km = "118000"
+var_doors = "3"
+var_pk = "110"
+var_cilinder = "1997"
+var_co2 = "198"
+var_euro = ""
+var_options = "4x4, ABS, Airbag, Alarm, Boordcomputer, Centrale vergrendeling, Elektrische buitenspiegels, Elektrische ramen voor, Metallic lak, Mistlampen, Radio & CD, Stuurbekrachtiging, Traction control, Trekhaak"
+var_carcolor = "Groen"
+var_interiorcolor = "Grijs"

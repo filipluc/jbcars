af529fa1be41b3a620708e54a07253506838ee4d
Filip Luchianov <filip.luchianov@gmail.com>
2018-12-16 11:41:02 +0200

    qq

---
+++ AddCars/cars/car112.py
@@ -0,0 +1,28 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "peugeot 208, 1.2 i benzine,Garantie,54000km/bwj.2014"
+var_categorie = "Auto's"
+var_brand = "Peugeot"
+var_model = "208"
+var_desc = "1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "6499"
+var_picspath = "car112"
+var_carroserie = "Berline 3"
+var_month = "nov"
+var_year = "2014"
+var_gas = "Benzine"
+var_transmissie = "Manu"
+var_km = "54000"
+var_doors = "3"
+var_pk = "82"
+var_cilinder = "1199"
+var_co2 = "104"
+var_euro = "euro 5"
+var_options = "ABS, Airbag, Alarm, Boordcomputer, Centrale vergrendeling, Elektrische buitenspiegels, Elektrische ramen voor, Metallic lak, Radio & CD, Stuurbekrachtiging, Traction control"
+var_carcolor = "Zilver"
+var_interiorcolor = "Grijs"

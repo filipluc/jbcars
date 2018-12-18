93c172e54cb6c818fded82e2bc2b2cc91cf32270
Filip Luchianov <filip.luchianov@gmail.com>
2018-12-18 15:41:56 +0200

    vacanta craciun 2018

--- AddCars/general.py
+++ AddCars/general.py
@@ -118,10 +118,11 @@ class GeneralFunctions():
         time.sleep(1)
         elem8 = driver.find_element_by_id("description")
         elem8.send_keys(car_detail.var_desc)
-        elem8.send_keys("\nopeningsuren\nmaandag tot vrijdag 10-18 uur\nzaterdag 10-15 uur\nadres\nLIERSESTEENWEG 153\n2547 LINT")
+        #elem8.send_keys("\nopeningsuren\nmaandag tot vrijdag 10-18 uur\nzaterdag 10-15 uur\nadres\nLIERSESTEENWEG 153\n2547 LINT")
         #elem8.send_keys("\n\nvoor de periode 16/07/2018 tot 26/07/2018\n\nGelieve mij te bellen als u gaat vertrekken om de wagen te bezichtigen\n\n")
         #elem8.send_keys("aarzel niet voor verdere inlichtigen en of afspraak tel 0485/673404\n\nwij zijn op vakantie\nvan 27/07/2018\ntot 11/08/2018")
-  
+        elem8.send_keys("\nVan 19/12/2018 tot 03/01/2019 is OPEN enkel op afspraak\n\nadres\nLIERSESTEENWEG 153\n2547 LINT")
+
         elem9 = driver.find_element_by_id('price')
         elem9.click()
         elem9.send_keys("Bedrag")

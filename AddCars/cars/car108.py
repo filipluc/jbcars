0de5155712b23ca0e430cebe66d43abdfbefd855
Filip Luchianov <filip.luchianov@gmail.com>
2019-01-04 16:34:42 +0200

    108 109

---
+++ AddCars/cars/car108.py
@@ -0,0 +1,28 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "vw fox 1.2 benzine ,Garantie"
+var_categorie = "Auto's"
+var_brand = "Volkswagen"
+var_model = "Fox"
+var_desc = "1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "2900"
+var_picspath = "car108"
+var_carroserie = "Berline 3"
+var_month = "feb"
+var_year = "2006"
+var_gas = "Benzine"
+var_transmissie = "Manu"
+var_km = "135000"
+var_doors = "3"
+var_pk = "55"
+var_cilinder = "1198"
+var_co2 = "146"
+var_euro = "euro 4"
+var_options = "ABS, Airbag, Boordcomputer, Radio & CD, Stuurbekrachtiging"
+var_carcolor = "Geel"
+var_interiorcolor = "Grijs"
---
+++ AddCars/cars/car109.py
@@ -0,0 +1,28 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "skoda fabia 1.2 benzine, Garantie, bwj. 2010, euro 5"
+var_categorie = "Auto's"
+var_brand = "Skoda"
+var_model = "Fabia"
+var_desc = "1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "4900"
+var_picspath = "car109"
+var_carroserie = "Berline 3"
+var_month = "jul"
+var_year = "2010"
+var_gas = "Benzine"
+var_transmissie = "Manu"
+var_km = "115000"
+var_doors = "5"
+var_pk = "59"
+var_cilinder = "1198"
+var_co2 = "132"
+var_euro = "euro 5"
+var_options = "ABS, Airbag, Alarm, Centrale vergrendeling, Elektrische ramen voor, Metallic lak, Radio & CD, Stuurbekrachtiging, Traction control"
+var_carcolor = "Blauw"
+var_interiorcolor = "Grijs"

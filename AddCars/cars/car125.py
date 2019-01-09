9648e0ba016b574fc315ffd0ea94e29498c6db4c
Filip Luchianov <filip.luchianov@gmail.com>
2019-01-09 14:44:56 +0200

    112 125

---
+++ AddCars/cars/car125.py
@@ -0,0 +1,28 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "opel corsa 1.3 cdti, bwj.12/2013,90000km,euro 5"
+var_categorie = "Auto's"
+var_brand = "Opel"
+var_model = "Corsa"
+var_desc = "zo meenemen ,voor export of handelaar\nkeuring=+150 euro\n\n"
+var_price = "4000"
+var_picspath = "car125"
+var_carroserie = "Berline 3"
+var_month = "dec"
+var_year = "2013"
+var_gas = "Diesel"
+var_transmissie = "Manu"
+var_km = "90000"
+var_doors = "5"
+var_pk = "75"
+var_cilinder = "1248"
+var_co2 = "112"
+var_euro = "euro 5"
+var_options = "ABS, Airbag, Airco, Alarm, Bluetooth, Boordcomputer, Centrale vergrendeling, Elektrische buitenspiegels, Elektrische ramen voor, Getint glas, Lichtmetalen velgen, Metallic lak, Navigatiesysteem, Radio & CD, Stuurbekrachtiging, Traction control"
+var_carcolor = "Grijs"
+var_interiorcolor = "Grijs"

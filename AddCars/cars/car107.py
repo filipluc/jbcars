0540da954e86e78043d36b58ee75a4a8568cad90
Filip Luchianov <filip.luchianov@gmail.com>
2019-01-19 13:08:53 +0200

    107

---
+++ AddCars/cars/car107.py
@@ -0,0 +1,28 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "renault trafic,1.9 d,lichte vracht,3 zit,prijs 2479 euro+btw"
+var_categorie = "Bestelwagens & Vrachtwagens"
+var_brand = "Renault"
+var_model = "Trafic"
+var_desc = "enkel voor export of handelaar\n\n2479 euro +btw/tva\ntrekhaak=3500 kg\nCAR PASS\ngekeurd met roos formulier=+150 euro\n"
+var_price = "3000"
+var_picspath = "car107"
+var_carroserie = "Bestelwagen"
+var_month = "maa"
+var_year = "2006"
+var_gas = "Diesel"
+var_transmissie = "Manu"
+var_km = "186000"
+var_doors = "4"
+var_pk = "82"
+var_cilinder = "1870"
+var_co2 = ""
+var_euro = ""
+var_options = "ABS, Airbag, Boordcomputer, Metallic lak, Radio & CD, Stuurbekrachtiging, Trekhaak"
+var_carcolor = "Zwart"
+var_interiorcolor = "Zwart"

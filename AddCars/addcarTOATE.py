6860f11789aecf6fb29d940492957b38e87f120a
Filip Luchianov <filip.luchianov@gmail.com>
2019-02-01 11:52:00 +0200

    102 122 removed

--- AddCars/addcarTOATE.py
+++ AddCars/addcarTOATE.py
@@ -15,7 +15,7 @@ from selenium.common.exceptions import TimeoutException
 from xmlrunner import *
 from AddCars.general import GeneralFunctions
 from AddCars.cars import car101
-from AddCars.cars import car102
+# from AddCars.cars import car102
 from AddCars.cars import car103
 from AddCars.cars import car104
 from AddCars.cars import car105
@@ -35,7 +35,7 @@ from AddCars.cars import car118
 from AddCars.cars import car119
 from AddCars.cars import car120
 from AddCars.cars import car121
-from AddCars.cars import car122
+# from AddCars.cars import car122
 from AddCars.cars import car123
 from AddCars.cars import car124
 # from AddCars.cars import car125
@@ -81,8 +81,8 @@ class AdaugaMasini(unittest.TestCase):
     def test_car101(self):
         GeneralFunctions.deleteAddCarFunction(self, car101)
     
-    def test_car102(self):
-        GeneralFunctions.deleteAddCarFunction(self, car102)
+    # def test_car102(self):
+    #     GeneralFunctions.deleteAddCarFunction(self, car102)
     
     def test_car103(self):
         GeneralFunctions.deleteAddCarFunction(self, car103)
@@ -141,8 +141,8 @@ class AdaugaMasini(unittest.TestCase):
     def test_car121(self):
         GeneralFunctions.deleteAddCarFunction(self, car121)
         
-    def test_car122(self):
-        GeneralFunctions.deleteAddCarFunction(self, car122)
+    # def test_car122(self):
+    #     GeneralFunctions.deleteAddCarFunction(self, car122)
 
     def test_car123(self):
         GeneralFunctions.deleteAddCarFunction(self, car123)

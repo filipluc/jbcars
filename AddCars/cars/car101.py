ceacbd997f07224274c43844a869508e3b5625e1
Filip Luchianov <filip.luchianov@gmail.com>
2019-02-11 18:25:43 +0200

    101

---
+++ AddCars/cars/car101.py
@@ -0,0 +1,30 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "opel astra 1.4 benzine,AUTOMAAT,garantie,euro 4"
+var_categorie = "Auto's"
+var_brand = "Opel"
+var_model = "Astra"
+var_desc = "1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "2900"
+var_picspath = "car101"
+var_carroserie = "Berline 2"
+var_month = "feb"
+var_year = "2003"
+var_gas = "Benzine"
+var_transmissie = "Auto"
+var_km = "114000"
+var_doors = "4"
+var_pk = "90"
+var_cilinder = "1389"
+var_co2 = "190"
+var_euro = "euro 4"
+var_options = "ABS, Airbag, Airco, Alarm, Automatische airco, Boordcomputer, Centrale vergrendeling, Elektrische ramen v + a, Getint glas, Lichtmetalen velgen, Metallic lak, Mistlampen, Parkeersensor, Radio & CD, Stuurbekrachtiging, Traction control"
+var_carcolor = "Zilver"
+var_interiorcolor = "Grijs"
+
+

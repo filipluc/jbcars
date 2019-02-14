a390256d20ce40451a80312e44ae6e0d8d56593c
Filip Luchianov <filip.luchianov@gmail.com>
2019-02-14 15:40:56 +0200

    121 122

---
+++ AddCars/cars/car121.py
@@ -0,0 +1,28 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "hyunndai santa fe 2.2 crdi,Garantie,Automaat,bwj.2008,/7 zit"
+var_categorie = "Auto's"
+var_brand = "Hyundai"
+var_model = "Santa Fe"
+var_desc = "1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "6900"
+var_picspath = "car121"
+var_carroserie = "SUV"
+var_month = "sep"
+var_year = "2008"
+var_gas = "Diesel"
+var_transmissie = "Auto"
+var_km = "176000"
+var_doors = "5"
+var_pk = "150"
+var_cilinder = "2188"
+var_co2 = "211"
+var_euro = "euro 4"
+var_options = "ABS, Airbag, Airco, Alarm, Automatische airco, Boordcomputer, Centrale vergrendeling, Elektrische buitenspiegels, Elektrische ramen v + a, Getint glas, Lederen bekleding, Lichtmetalen velgen, Metallic lak, Mistlampen, Radio & CD, Stuurbekrachtiging, Verwarmde buitenspiegels"
+var_carcolor = "Zwart"
+var_interiorcolor = "Zwart"
---
+++ AddCars/cars/car122.py
@@ -0,0 +1,28 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "peugeot partner 1.4 benzine,Garantie,bwj.2008/110000km/airco"
+var_categorie = "Auto's"
+var_brand = "Peugeot"
+var_model = "Partner"
+var_desc = "1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "4900"
+var_picspath = "car122"
+var_carroserie = "Monovolume"
+var_month = "feb"
+var_year = "2008"
+var_gas = "Benzine"
+var_transmissie = "Manu"
+var_km = "110000"
+var_doors = "5"
+var_pk = "75"
+var_cilinder = "1360"
+var_co2 = "175"
+var_euro = "euro 4"
+var_options = "ABS, Airbag, Airco, Alarm, Boordcomputer, Centrale vergrendeling, Elektrische buitenspiegels, Elektrische ramen voor, Getint glas, Metallic lak, Radio & CD, Stuurbekrachtiging, Traction control, Verwarmde buitenspiegels"
+var_carcolor = "Goud"
+var_interiorcolor = "Grijs"

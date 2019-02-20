044bf34fcb85650434c44ffb00f2a13635fd1922
Filip Luchianov <filip.luchianov@gmail.com>
2019-02-20 14:25:22 +0200

    125

---
+++ AddCars/cars/car125.py
@@ -0,0 +1,28 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "citroen berlingo,1.6 benzine,3zit,airco,bwj.2014/50000 km/LV"
+var_categorie = "Bestelwagens & Vrachtwagens"
+var_brand = "Citroen"
+var_model = "Berlingo"
+var_desc = "1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "7900"
+var_picspath = "car125"
+var_carroserie = "Bestelauto"
+var_month = "apr"
+var_year = "2014"
+var_gas = "Benzine"
+var_transmissie = "Manu"
+var_km = "50000"
+var_doors = "4"
+var_pk = "98"
+var_cilinder = "1598"
+var_co2 = ""
+var_euro = "euro 5"
+var_options = "ABS, Airbag, Airco, Alarm, Boordcomputer, Centrale vergrendeling, Elektrische buitenspiegels, Elektrische ramen voor, Radio & CD, Stuurbekrachtiging, Traction control"
+var_carcolor = "Wit"
+var_interiorcolor = ""

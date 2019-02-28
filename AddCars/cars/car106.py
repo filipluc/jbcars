07b99286fc09c508885d1b90f01041a70e321b15
Filip Luchianov <filip.luchianov@crfhealth.com>
2019-02-28 14:07:22 +0200

    111

---
+++ AddCars/cars/car106.py
@@ -0,0 +1,28 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "mercedes v klasse,double cabine 5 zit,l.v.,bwj.2014/43000 km"
+var_categorie = "Auto's"
+var_brand = "Mercedes"
+var_model = "V Klasse"
+var_desc = "20500 € +btw/tva=24805 €\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "24805"
+var_picspath = "car106"
+var_carroserie = "Monovolume"
+var_month = "okt"
+var_year = "2014"
+var_gas = "Diesel"
+var_transmissie = "Manu"
+var_km = "43000"
+var_doors = "5"
+var_pk = "163"
+var_cilinder = "2143"
+var_co2 = "149"
+var_euro = "euro 6"
+var_options = "ABS, Airbag, Airco, Alarm, Automatische airco, Bluetooth, Boordcomputer, Camera, Centrale vergrendeling, Cruise control, Elektrische buitenspiegels, Elektrische ramen voor, Getint glas, Metallic lak, Navigatiesysteem, Radio & CD, Stuurbekrachtiging, Traction control, Trekhaak, Verwarmde buitenspiegels"
+var_carcolor = "Bruin"
+var_interiorcolor = "Grijs"

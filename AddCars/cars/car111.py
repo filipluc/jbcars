b24ffae54f20789e16c4dac67a627e9209bd0466
Filip Luchianov <filip.luchianov@crfhealth.com>
2019-03-28 16:46:50 +0200

    111

---
+++ AddCars/cars/car111.py
@@ -0,0 +1,28 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "ford transit 2,2 tdci,minibus 8 pl,car pass"
+var_categorie = "Auto's"
+var_brand = "Ford"
+var_model = "Transit"
+var_desc = "export of handelaar\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "6500"
+var_picspath = "car111"
+var_carroserie = "Monovolume"
+var_month = "sep"
+var_year = "2007"
+var_gas = "Diesel"
+var_transmissie = "Manu"
+var_km = "192000"
+var_doors = "4"
+var_pk = "109"
+var_cilinder = "2198"
+var_co2 = "208"
+var_euro = "euro 4"
+var_options = "ABS, Airbag, Airco, Alarm, Boordcomputer, Centrale vergrendeling, Cruise control, Elektrische buitenspiegels, Elektrische ramen voor, Getint glas, Metallic lak, Radio & CD, Stuurbekrachtiging, Traction control, Verwarmde buitenspiegels"
+var_carcolor = "Groen"
+var_interiorcolor = "Grijs"

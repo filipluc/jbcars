23d7978764b60b079eb0df6028b31955a8ac1218
Filip Luchianov <filip.luchianov@crfhealth.com>
2019-04-05 18:54:11 +0300

    112 120

---
+++ AddCars/cars/car112.py
@@ -0,0 +1,28 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "opel astra 1.3 cdti break,Garantie,bwj.12/2009/103000km/navi"
+var_categorie = "Auto's"
+var_brand = "Opel"
+var_model = "Astra"
+var_desc = "1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "3500"
+var_picspath = "car112"
+var_carroserie = "Break"
+var_month = "dec"
+var_year = "2009"
+var_gas = "Diesel"
+var_transmissie = "Manu"
+var_km = "100000"
+var_doors = "5"
+var_pk = "90"
+var_cilinder = "1248"
+var_co2 = "130"
+var_euro = "euro 4"
+var_options = "ABS, Adaptieve lichten, Airbag, Airco, Alarm, Automatische airco, Bluetooth, Boordcomputer, Centrale vergrendeling, Cruise control, Elektrische buitenspiegels, Elektrische ramen voor, Getint glas, Metallic lak, Mistlampen, Navigatiesysteem, Radio & CD, Stuurbekrachtiging, Traction control, Trekhaak, Verwarmde buitenspiegels"
+var_carcolor = "Groen"
+var_interiorcolor = "Grij"

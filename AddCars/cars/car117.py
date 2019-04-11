a522bf2c8ae564a961c16848b18640aad277d977
Filip Luchianov <filip.luchianov@crfhealth.com>
2019-04-11 14:46:09 +0300

    117

---
+++ AddCars/cars/car117.py
@@ -0,0 +1,28 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "vw golf 1.9 tdi, Garantie,5 deurs,airco,CAR PASS"
+var_categorie = "Auto's"
+var_brand = "Volkswagen"
+var_model = "Golf"
+var_desc = "1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "4900"
+var_picspath = "car117"
+var_carroserie = "Berline 3"
+var_month = "feb"
+var_year = "2008"
+var_gas = "Diesel"
+var_transmissie = "Manu"
+var_km = "179000"
+var_doors = "5"
+var_pk = "105"
+var_cilinder = "1896"
+var_co2 = "135"
+var_euro = "euro 4"
+var_options = "ABS, Airbag, Airco, Alarm, Automatische airco, Boordcomputer, Centrale vergrendeling, Elektrische buitenspiegels, Elektrische ramen voor, Getint glas, Metallic lak, Radio & CD, Stuurbekrachtiging, Traction control"
+var_carcolor = "Zilver"
+var_interiorcolor = "Grijs"

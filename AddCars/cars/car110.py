08d0483b84db87b6a8a9bdc33d71636cfd50ae44
Filip Luchianov <filip.luchianov@crfhealth.com>
2019-04-19 16:20:54 +0300

    110

---
+++ AddCars/cars/car110.py
@@ -0,0 +1,28 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "opel astra 1.7 cdti break,Garantie"
+var_categorie = "Auto's"
+var_brand = "Opel"
+var_model = "Astra"
+var_desc = "1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "2999"
+var_picspath = "car110"
+var_carroserie = "Break"
+var_month = "okt"
+var_year = "2005"
+var_gas = "Diesel"
+var_transmissie = "Manu"
+var_km = "163000"
+var_doors = "5"
+var_pk = "100"
+var_cilinder = "1686"
+var_co2 = "135"
+var_euro = "euro 4"
+var_options = "ABS, Airbag, Airco, Alarm, Boordcomputer, Centrale vergrendeling, Elektrische buitenspiegels, Elektrische ramen voor, Getint glas, Metallic lak, Radio & CD, Stuurbekrachtiging, Traction control, Trekhaak"
+var_carcolor = "Grijs"
+var_interiorcolor = "Grijs"

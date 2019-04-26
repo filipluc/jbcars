557ad275a07baec2171587e164220a73630a134b
Filip Luchianov <filip.luchianov@crfhealth.com>
2019-04-26 11:36:08 +0300

    123

---
+++ AddCars/cars/car123.py
@@ -0,0 +1,28 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "mini cooper D coupe PACEMAN,1.6 d,Garantie,CAR PASS,bwj.2014"
+var_categorie = "Auto's"
+var_brand = "Mini"
+var_model = "Paceman"
+var_desc = "1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "11999"
+var_picspath = "car123"
+var_carroserie = "Berline 3"
+var_month = "feb"
+var_year = "2014 "
+var_gas = "Diesel"
+var_transmissie = "Manu"
+var_km = "90000"
+var_doors = "3"
+var_pk = "110"
+var_cilinder = "1598"
+var_co2 = "115"
+var_euro = "euro 6"
+var_options = "ABS, Airbag, Airco, Alarm, Automatische airco, Bluetooth, Boordcomputer, Centrale vergrendeling, Elektrische buitenspiegels, Elektrische ramen voor, Getint glas, Lederen bekleding, Lichtmetalen velgen, Metallic lak, Mistlampen, Panoramisch dak, Radio & CD, Stuurbekrachtiging, Traction control, Verwarmde buitenspiegels"
+var_carcolor = "Wit"
+var_interiorcolor = "Zwart"

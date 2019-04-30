8dffa97b0a7719ecf7db784ccd42cb53ed3c0a3c
Filip Luchianov <filip.luchianov@crfhealth.com>
2019-05-01 02:40:05 +0300

    125

---
+++ AddCars/cars/car125.py
@@ -0,0 +1,28 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "mercedes GLC 250d 4MATIC coupe,204PK,bwj2017/garantie,euro6b"
+var_categorie = "Auto's"
+var_brand = "Mercedes"
+var_model = "GLC Klasse"
+var_desc = "1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "44444"
+var_picspath = "car125"
+var_carroserie = "Coupe"
+var_month = "jan"
+var_year = "2017"
+var_gas = "Diesel"
+var_transmissie = "Auto"
+var_km = "97000"
+var_doors = "5"
+var_pk = "204"
+var_cilinder = "2143"
+var_co2 = "143"
+var_euro = "euro 6"
+var_options = "4x4, ABS, Adaptieve lichten, Airbag, Airco, Alarm, Automatische airco, Bluetooth, Boordcomputer, Camera, Centrale vergrendeling, Cruise control, Elektrische buitenspiegels, Elektrische ramen v + a, Elektrische zetelverstelling, Getint glas, Lederen bekleding, Lichtmetalen velgen, Metallic lak, Mistlampen, Navigatiesysteem, Parkeersensor, Radio & CD, Sportpakket, Start/Stop systeem, Startonderbreker, Stuurbekrachtiging, Traction control, Verwarmde buitenspiegels, Xenon verlichting, Zetelverwarming"
+var_carcolor = "Bruin"
+var_interiorcolor = "Zwart"

33e4ad72f28e939635424a216e9e4c4bd726325b
Filip Luchianov <filip.luchianov@crfhealth.com>
2019-05-07 10:38:17 +0300

    124

---
+++ AddCars/cars/car124.py
@@ -0,0 +1,28 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "opel corsa 1.3 cdti ,Garantie,CAR PASS,airco"
+var_categorie = "Auto's"
+var_brand = "Opel"
+var_model = "Corsa"
+var_desc = "1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n\n"
+var_price = "2999"
+var_picspath = "car124"
+var_carroserie = "Berline 3"
+var_month = "aug"
+var_year = "2008"
+var_gas = "Diesel"
+var_transmissie = "Manu"
+var_km = "150000"
+var_doors = "3"
+var_pk = "75"
+var_cilinder = "1248"
+var_co2 = "119"
+var_euro = "euro 4"
+var_options = "ABS, Airbag, Airco, Alarm, Boordcomputer, Centrale vergrendeling, Elektrische buitenspiegels, Elektrische ramen voor, Getint glas, Lichtmetalen velgen, Metallic lak, Radio & CD, Stuurbekrachtiging, Traction control"
+var_carcolor = "Zwart"
+var_interiorcolor = "Grijs"

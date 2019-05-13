a0523ffa475f079aea7797a2896c3a3c1985bc5a
Filip Luchianov <filip.luchianov@crfhealth.com>
2019-05-13 20:15:08 +0300

    qq

--- AddCars/addcarTOATE.py
+++ AddCars/addcarTOATE.py
@@ -34,13 +34,13 @@ from AddCars.cars import car117
 from AddCars.cars import car118
 from AddCars.cars import car119
 from AddCars.cars import car120
-from AddCars.cars import car121
+# from AddCars.cars import car121
 from AddCars.cars import car122
 from AddCars.cars import car123
 from AddCars.cars import car124
 from AddCars.cars import car125
 from AddCars.cars import car126
-from AddCars.cars import car127
+# from AddCars.cars import car127
 from AddCars.cars import car128
 from AddCars.cars import car129
 from AddCars.cars import car130
@@ -141,8 +141,8 @@ class AdaugaMasini(unittest.TestCase):
     def test_car120(self):
         GeneralFunctions.deleteAddCarFunction(self, car120)
         
-    def test_car121(self):
-        GeneralFunctions.deleteAddCarFunction(self, car121)
+    # def test_car121(self):
+    #     GeneralFunctions.deleteAddCarFunction(self, car121)
         
     def test_car122(self):
         GeneralFunctions.deleteAddCarFunction(self, car122)
@@ -159,8 +159,8 @@ class AdaugaMasini(unittest.TestCase):
     def test_car126(self):
         GeneralFunctions.deleteAddCarFunction(self, car126)
 
-    def test_car127(self):
-        GeneralFunctions.deleteAddCarFunction(self, car127)
+    # def test_car127(self):
+    #     GeneralFunctions.deleteAddCarFunction(self, car127)
 
     def test_car128(self):
         GeneralFunctions.deleteAddCarFunction(self, car128)

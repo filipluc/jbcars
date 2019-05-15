dc640fed4e25db84f7cbe3c818a34ca9a0de2120
Filip Luchianov <filip.luchianov@crfhealth.com>
2019-05-15 21:30:14 +0300

    2 cars

---
+++ AddCars/cars/car121.py
@@ -0,0 +1,28 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "citroen jumpy 2.0 hdi,Garantie,CAR PASS,monovolume 8 zetels"
+var_categorie = "Auto's"
+var_brand = "Citroen"
+var_model = "Jumpy"
+var_desc = "juist groot onderhoud :nieuwe koppeling en vliegwiel+ distributieriem en waterpomp\n\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "4999"
+var_picspath = "car121"
+var_carroserie = "Monovolume"
+var_month = "apr"
+var_year = "2008"
+var_gas = "Diesel"
+var_transmissie = "Manu"
+var_km = "192000"
+var_doors = "4"
+var_pk = "120"
+var_cilinder = "1997"
+var_co2 = "198"
+var_euro = "euro 4"
+var_options = "ABS, Airbag, Alarm, Boordcomputer, Centrale vergrendeling, Elektrische buitenspiegels, Elektrische ramen voor, Getint glas, Metallic lak, Radio & CD, Stuurbekrachtiging, Traction control"
+var_carcolor = "Grijs"
+var_interiorcolor = "Grijs"
---
+++ AddCars/cars/car127.py
@@ -0,0 +1,28 @@
+'''
+Created on Jun 3, 2018
+
+@author: Filip.LUCHIANOV
+'''
+
+
+var_title = "mini cooper s roadster 1.6 benzine,bwj. 2012/71000km,euro 5"
+var_categorie = "Auto's"
+var_brand = "Mini"
+var_model = "Roadster"
+var_desc = "meer info 0479/237751\n"
+var_price = "16900"
+var_picspath = "car127"
+var_carroserie = "Cabriolet"
+var_month = "jun"
+var_year = "2012"
+var_gas = "Benzine"
+var_transmissie = "Manu"
+var_km = "71000"
+var_doors = "2"
+var_pk = "163"
+var_cilinder = "1598"
+var_co2 = "139"
+var_euro = "Euro 5"
+var_options = "ABS, Adaptieve lichten, Airbag, Airco, Alarm, Automatische airco, Bluetooth, Boordcomputer, Centrale vergrendeling, Elektrische buitenspiegels, Elektrische ramen voor, Getint glas, Lederen bekleding, Lichtmetalen velgen, Metallic lak, Mistlampen, Navigatiesysteem, Parkeersensor, Radio & CD, Stuurbekrachtiging, Traction control, Verwarmde buitenspiegels, Xenon verlichting, Zetelverwarming"
+var_carcolor = "Zwart"
+var_interiorcolor = "Beige"

bc152bb0fe592cbbf5eb50c403b75e9e5e366d86
Filip Luchianov <filip.luchianov@crfhealth.com>
2019-06-09 19:34:24 +0300

    105

--- AddCars/cars/car105.py
+++ AddCars/cars/car105.py
@@ -5,35 +5,38 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "skoda roomster 1.4 benzine,Garantie,CAR PASS,airco,bwj.2009"
+var_title = "volvo v50,2.0 d,1 jaar Garantie,CAR PASS,nieuwe staat"
 var_categorie = "Auto's"
-var_brand = "Skoda"
-var_model = "Roomster"
+var_brand = "Volvo"
+var_model = "V50"
 var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "3999"
+var_price = "2999"
 var_picspath = "car105"
-var_carroserie = "Monovolume"
-var_month = "maa"
-var_year = "2009"
-var_gas = "Benzine"
+var_carroserie = "Break"
+var_month = ""
+var_year = "2004"
+var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "110000"
+var_km = "165000"
 var_doors = "5"
-var_pk = "85"
-var_cilinder = "1390"
-var_co2 = "160"
-var_euro = "euro 4"
+var_pk = ""
+var_cilinder = "1997"
+var_co2 = ""
+var_euro = ""
 var_options = """Cruise Control
 Boordcomputer
+Climate control
 Centrale vergrendeling
 Elektrische buitenspiegels
-Airconditioning
 Traction-control
 Elektrische ramen
 ABS
+Mistlampen
 Airbags
+Isofix
 Alarm
 Radio
-Metaalkleur"""
-var_carcolor = "Zwart"
+Lichtmetalen velgen
+Trekhaak"""
+var_carcolor = "Grijs"
 var_interiorcolor = "Grijs"

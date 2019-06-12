a4d14657d416be308f80418abf6dff7eaa342dcf
Filip Luchianov <filip.luchianov@crfhealth.com>
2019-06-12 17:18:07 +0300

    aa

--- AddCars/cars/car101.py
+++ AddCars/cars/car101.py
@@ -5,37 +5,35 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "toyota corolla 1.4 d4d,Garantie,CAR PASS"
+var_title = "kia sportage 2.0 d,Garantie,CAR PASS"
 var_categorie = "Auto's"
-var_brand = "Toyota"
-var_model = "Corolla"
+var_brand = "Kia"
+var_model = "Sportage"
 var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "2999"
+var_price = "3999"
 var_picspath = "car101"
-var_carroserie = "Stadsauto"
-var_month = "feb"
-var_year = "2007"
+var_carroserie = "SUV"
+var_month = ""
+var_year = "2008"
 var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "159000"
-var_doors = "5"
-var_pk = "90"
-var_cilinder = "1364"
-var_co2 = "128"
-var_euro = "euro 4"
+var_km = "125000"
+var_doors = ""
+var_pk = ""
+var_cilinder = "1991"
+var_co2 = ""
+var_euro = ""
 var_options = """Boordcomputer
 Centrale vergrendeling
 Elektrische buitenspiegels
-Airconditioning
 Traction-control
-Parkeersensor
 Elektrische ramen
 ABS
 Airbags
 Alarm
 Radio
 Lichtmetalen velgen
-Metaalkleur"""
+Trekhaak"""
 var_carcolor = "Zwart"
 var_interiorcolor = "Grijs"
 
--- AddCars/cars/car120.py
+++ AddCars/cars/car120.py
@@ -5,24 +5,47 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "toyota rav 4, D4D,4x4,garantie,leder,€5784+btw,100000 km"
+var_title = "opel insignia 2.0 d,AUTOMAAT,euro 5"
 var_categorie = "Auto's"
-var_brand = "Toyota"
-var_model = "RAV 4"
-var_desc = "\nprijs: 5784 € + btw/tva\n\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "6999"
+var_brand = "Opel"
+var_model = "Insignia"
+var_desc = "\nexport of handelaar\n"
+var_price = "5000"
 var_picspath = "car120"
-var_carroserie = "SUV"
-var_month = "aug"
-var_year = "2008"
+var_carroserie = "Berline"
+var_month = ""
+var_year = "2010"
 var_gas = "Diesel"
-var_transmissie = "Manu"
-var_km = "100000"
-var_doors = "5"
-var_pk = "136"
-var_cilinder = "2231"
-var_co2 = "175"
-var_euro = "euro 4"
-var_options = "4x4, ABS, Airbag, Airco, Alarm, Automatische airco, Bluetooth, Boordcomputer, Centrale vergrendeling, Cruise control, Elektrische buitenspiegels, Elektrische ramen v + a, Getint glas, Lederen bekleding, Lichtmetalen velgen, Metallic lak, Mistlampen, Parkeersensor, Radio & CD, Stuurbekrachtiging, Traction control, Trekhaak, Verwarmde buitenspiegels"
+var_transmissie = "Auto"
+var_km = "273000"
+var_doors = ""
+var_pk = ""
+var_cilinder = "1956"
+var_co2 = ""
+var_euro = ""
+var_options = """Cruise Control
+Navigatiesysteem
+Elektrische buitenspiegels
+Elektrische stoelverstelling
+Parkeersensor
+Elektrische ramen
+Electronic Stability Program (ESP)
+Adaptieve lichten
+Alarm
+Lichtmetalen velgen
+Metaalkleur
+Lederen bekleding
+Boordcomputer
+Climate control
+Centrale vergrendeling
+Airconditioning
+Traction-control
+Verwarmde buitenspiegels
+Xenon verlichting
+ABS
+Mistlampen
+Airbags
+Zetelverwarming
+Radio"""
 var_carcolor = "Grijs"
-var_interiorcolor = "Grijs"
+var_interiorcolor = "Zwart"

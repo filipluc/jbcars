b4154566916608a36fae1bcb62e020d4bdf2e645
Filip Luchianov <filip.luchianov@crfhealth.com>
2019-07-01 17:40:25 +0300

    105

--- AddCars/cars/car105.py
+++ AddCars/cars/car105.py
@@ -5,38 +5,39 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "volvo v50,2.0 d,1 jaar Garantie,CAR PASS,nieuwe staat"
+var_title = "renault laguna coupe 2.0 dci,Garantie,CAR PASS"
 var_categorie = "Auto's"
-var_brand = "Volvo"
-var_model = "V50"
+var_brand = "Renault"
+var_model = "Laguna"
 var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "2999"
+var_price = "5900"
 var_picspath = "car105"
-var_carroserie = "Break"
+var_carroserie = "Coupe"
 var_month = ""
-var_year = "2004"
+var_year = "2008"
 var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "165000"
-var_doors = "5"
+var_km = "139000"
+var_doors = "3"
 var_pk = ""
-var_cilinder = "1997"
-var_co2 = ""
-var_euro = ""
+var_cilinder = "1995"
+var_co2 = "155"
+var_euro = "euro 4"
 var_options = """Cruise Control
 Boordcomputer
 Climate control
 Centrale vergrendeling
+Navigatiesysteem
 Elektrische buitenspiegels
 Traction-control
+Parkeersensor
+Verwarmde buitenspiegels
 Elektrische ramen
 ABS
 Mistlampen
 Airbags
-Isofix
 Alarm
 Radio
-Lichtmetalen velgen
-Trekhaak"""
+Lichtmetalen velgen"""
 var_carcolor = "Grijs"
 var_interiorcolor = "Grijs"

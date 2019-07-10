664b6a78be9d2a0255045a7e4b8a556a33bdd0c9
Filip Luchianov <filip.luchianov@crfhealth.com>
2019-07-10 15:12:19 +0300

    106

--- AddCars/cars/car106.py
+++ AddCars/cars/car106.py
@@ -5,32 +5,34 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "peugeot partner 1.6 hdi,Garantie,bwj.2009/km 112000"
+var_title = "peugeot 206+,1.1 benzine,Garantie,CAR PASS,97000 kmbwj.2010"
 var_categorie = "Auto's"
 var_brand = "Peugeot"
-var_model = "Partner Tepee"
+var_model = "206"
 var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "4999"
+var_price = "4500"
 var_picspath = "car106"
-var_carroserie = "Monovolume"
-var_month = "feb"
-var_year = "2009"
-var_gas = "Diesel"
+var_carroserie = "Stasauto"
+var_month = ""
+var_year = "2010"
+var_gas = "Benzine"
 var_transmissie = "Hand"
-var_km = "112000"
+var_km = "97000"
 var_doors = "5"
-var_pk = "75"
-var_cilinder = "1560"
-var_co2 = "150"
+var_pk = ""
+var_cilinder = "1124"
+var_co2 = "135"
 var_euro = "euro 4"
 var_options = """Boordcomputer
 Centrale vergrendeling
+Elektrische buitenspiegels
 Airconditioning
 Elektrische ramen
 ABS
+Electronic Stability Program (ESP)
 Airbags
 Alarm
 Radio
 Metaalkleur"""
-var_carcolor = "Blauw"
+var_carcolor = "Grijs"
 var_interiorcolor = "Grijs"

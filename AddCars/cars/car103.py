ae127210802c6f454ccb2c02dab5dc8dd0ede635
Filip Luchianov <filip.luchianov@crfhealth.com>
2019-09-25 20:23:37 +0300

    103

--- AddCars/cars/car103.py
+++ AddCars/cars/car103.py
@@ -5,34 +5,38 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "ford c max 1.6 hdi, airco, export"
+var_title = "dacia lodgy 1.2 benzine,airco,navi,euro 5"
 var_categorie = "Auto's"
-var_brand = "Ford"
-var_model = "C-Max"
-var_desc = "\nexport of handelaar\n"	
-var_price = "1500"
+var_brand = "Dacia"
+var_model = "Lodgy"
+var_desc = "\nenkel voor export of handelaar\n"	
+var_price = "5000"
 var_picspath = "car103"
 var_carroserie = "Monovolume"
-var_month = "jul"
-var_year = "2004"
-var_gas = "Diesel"
+var_month = ""
+var_year = "2013"
+var_gas = "Benzine"
 var_transmissie = "Hand"
-var_km = "238000"
+var_km = "114000"
 var_doors = "5"
-var_pk = "110"
-var_cilinder = "1560"
-var_co2 = "129"
-var_euro = "euro 3"
-var_options = """Boordcomputer
+var_pk = ""
+var_cilinder = "1197"
+var_co2 = "135"
+var_euro = "euro 5"
+var_options = """Cruise Control
+Bluetooth
+Boordcomputer
 Centrale vergrendeling
-Airconditioning
+Navigatiesysteem
+Elektrische buitenspiegels
 Traction-control
+Verwarmde buitenspiegels
+USB
 Elektrische ramen
 ABS
 Airbags
+Isofix
 Alarm
-Radio
-Trekhaak
-Metaalkleur"""
-var_carcolor = "Zilver"
-var_interiorcolor = "Grijs"
+Radio"""
+var_carcolor = ""
+var_interiorcolor = ""

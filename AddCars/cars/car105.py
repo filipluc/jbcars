480a21a0828dceb5d8b557202d8779a30ffbe5ac
Filip Luchianov <filip.luchianov@signanthealth.com>
2019-10-04 14:56:53 +0300

    105

--- AddCars/cars/car105.py
+++ AddCars/cars/car105.py
@@ -5,38 +5,52 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "peugeot partner 1.6hdi,Garantie,CAR PASS,bwj2012/euro 5"
+var_title = "lexus is 220 d,Garantie,CAR PASS"
 var_categorie = "Auto's"
-var_brand = "Peugeot"
-var_model = "Partner"
+var_brand = "Lexus"
+var_model = "IS"
 var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "6500"
+var_price = "6900"
 var_picspath = "car105"
-var_carroserie = "Monovolume"
+var_carroserie = "Berline"
 var_month = ""
-var_year = "2012"
+var_year = "2010"
 var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "130000"
+var_km = "169000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1560"
-var_co2 = "135"
-var_euro = "euro 5"
-var_options = """Boordcomputer
-Centrale vergrendeling
+var_cilinder = "2231"
+var_co2 = "148"
+var_euro = "euro 4"
+var_options = """Cruise Control
+Startonderbreker
+Bluetooth
+Parkeercamera
+Navigatiesysteem
 Elektrische buitenspiegels
-Schuifdeur
-Airconditioning
-Traction-control
+Elektrische stoelverstelling
+Parkeersensor
 Elektrische ramen
-ABS
 Electronic Stability Program (ESP)
-Airbags
+Adaptieve lichten
 Isofix
 Alarm
+Metaalkleur
+Lederen bekleding
+Boordcomputer
+Climate control
+Centrale vergrendeling
+Airconditioning
+Start-stop-systeem
+Traction-control
+Verwarmde buitenspiegels
+Xenon verlichting
+ABS
+Mistlampen
+Airbags
+Zetelverwarming
 Radio
-Trekhaak
-Metaalkleur"""
+Trekhaak"""
 var_carcolor = ""
 var_interiorcolor = ""

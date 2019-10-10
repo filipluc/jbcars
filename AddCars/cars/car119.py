12123fc4b00df53e285bc8eb29b151845dc4637f
Filip Luchianov <filip.luchianov@signanthealth.com>
2019-10-10 14:14:37 +0300

    119

--- AddCars/cars/car119.py
+++ AddCars/cars/car119.py
@@ -5,37 +5,46 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "kia cee d 1.4 benzine,Garantie,CAR PASS,airco"
+var_title = "opel vectra 1.9 cdti,Garantie,CAR PASS,70.000 km !!!!"
 var_categorie = "Auto's"
-var_brand = "Kia"
-var_model = "(Pro) Cee d"
+var_brand = "Opel"
+var_model = "Vectra"
 var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "4500"
+var_price = "3999"
 var_picspath = "car119"
-var_carroserie = "Stadsauto"
+var_carroserie = "Berline"
 var_month = ""
-var_year = "2009"
-var_gas = "Benzine"
+var_year = "2006"
+var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "138000"
-var_doors = "5"
+var_km = "70000"
+var_doors = ""
 var_pk = ""
-var_cilinder = "1396"
-var_co2 = "137"
+var_cilinder = "1910"
+var_co2 = "154"
 var_euro = "euro 4"
-var_options = """Boordcomputer
-Centrale vergrendeling
+var_options = """Cruise Control
+Bluetooth
 Elektrische buitenspiegels
+Open / panorama dak
+Parkeersensor
+Elektrische ramen
+Electronic Stability Program (ESP)
+Adaptieve lichten
+Alarm
+Lichtmetalen velgen
+Metaalkleur
+Lederen bekleding
+Boordcomputer
+Climate control
+Centrale vergrendeling
 Airconditioning
 Traction-control
 Verwarmde buitenspiegels
-Elektrische ramen
 ABS
-Electronic Stability Program (ESP)
+Mistlampen
 Airbags
-Isofix
-Alarm
-Radio
-Metaalkleur"""
+Zetelverwarming
+Radio"""
 var_carcolor = ""
 var_interiorcolor = ""

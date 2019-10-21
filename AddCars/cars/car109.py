dea81e66b9dfaf317719bb6a473c324a56d216bd
Filip Luchianov <filip.luchianov@signanthealth.com>
2019-10-21 12:38:10 +0300

    109

--- AddCars/cars/car109.py
+++ AddCars/cars/car109.py
@@ -5,36 +5,36 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "opel corsa 1.0 benzine,Garantie,CAR PASS,euro 6b"
+var_title = "opel zafira 1.9 d,Garantie,CAR PASS,7 zit"
 var_categorie = "Auto's"
 var_brand = "Opel"
-var_model = "Corsa"
+var_model = "Zafira"
 var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "6999"
+var_price = "4500"
 var_picspath = "car109"
-var_carroserie = "Stadsauto"
+var_carroserie = "Monovolume"
 var_month = ""
-var_year = "2015"
-var_gas = "Benzine"
+var_year = "2007"
+var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "71000"
+var_km = "110000"
 var_doors = "5"
 var_pk = ""
-var_cilinder = "999"
-var_co2 = "166"
-var_euro = "Euro 6"
-var_options = """Bluetooth
-Boordcomputer
+var_cilinder = "1910"
+var_co2 = "162"
+var_euro = "Euro 4"
+var_options = """Boordcomputer
 Centrale vergrendeling
 Elektrische buitenspiegels
 Traction-control
+Parkeersensor
 Verwarmde buitenspiegels
-USB
 Elektrische ramen
 ABS
 Airbags
-Isofix
 Alarm
-Radio"""
+Radio
+Lichtmetalen velgen
+Trekhaak"""
 var_carcolor = ""
 var_interiorcolor = ""

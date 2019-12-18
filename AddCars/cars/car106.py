c2665a7fc83cc502383de1eb99dd85e7c96880c0
Filip Luchianov <filip.luchianov@signanthealth.com>
2019-12-18 17:13:40 +0200

    106

--- AddCars/cars/car106.py
+++ AddCars/cars/car106.py
@@ -5,28 +5,36 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "fiat ducato 2.3 jtd,euro 5,MOTOR CAPOT,93000 km,bj2012,7 zit"
+var_title = "citroen berlingo 1.6 d,Garantie,CAR PASS,euro 5"
 var_categorie = "Auto's"
-var_brand = "Fiat"
-var_model = "Ducato"
-var_desc = "\nmotor kapot !!!!\n7 zit\neuro 5\ntrekhaak =3000 kg\nprijs 4500 EUR + btw/tva\n"
-var_price = "5445"
+var_brand = "Citroën"
+var_model = "Berlingo"
+var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "6999"
 var_picspath = "car106"
 var_carroserie = ""
 var_month = ""
-var_year = "2012"
+var_year = "2013"
 var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "92000"
+var_km = "108000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "2287"
-var_co2 = "195"
+var_cilinder = "1560"
+var_co2 = "135"
 var_euro = "euro 5"
-var_options = """ABS
+var_options = """Boordcomputer
 Centrale vergrendeling
+Elektrische buitenspiegels
+Traction-control
+Verwarmde buitenspiegels
+Elektrische ramen
+ABS
+Mistlampen
+Electronic Stability Program (ESP)
+Airbags
+Isofix
 Alarm
-Radio
-Trekhaak"""
+Radio"""
 var_carcolor = ""
 var_interiorcolor = ""

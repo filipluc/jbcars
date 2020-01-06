c9f9f48444500c0a5bcd2a9557f284c86ca5dfa2
Filip Luchianov <filip.luchianov@signanthealth.com>
2020-01-06 14:59:01 +0200

    aa

--- AddCars/addcarTOATE.py
+++ AddCars/addcarTOATE.py
@@ -117,8 +117,8 @@ class AdaugaMasini(unittest.TestCase):
     def test_car112(self):
         GeneralFunctions.deleteAddCarFunction(self, car112)
         
-    def test_car113(self):
-        GeneralFunctions.deleteAddCarFunction(self, car113)
+    # def test_car113(self):
+    #     GeneralFunctions.deleteAddCarFunction(self, car113)
     
     def test_car114(self):
         GeneralFunctions.deleteAddCarFunction(self, car114)
@@ -135,8 +135,8 @@ class AdaugaMasini(unittest.TestCase):
     def test_car118(self):
         GeneralFunctions.deleteAddCarFunction(self, car118)
         
-    def test_car119(self):
-        GeneralFunctions.deleteAddCarFunction(self, car119)
+    # def test_car119(self):
+    #     GeneralFunctions.deleteAddCarFunction(self, car119)
 
     def test_car120(self):
         GeneralFunctions.deleteAddCarFunction(self, car120)

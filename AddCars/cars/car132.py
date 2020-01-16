42c12a994976e460e62dec9be3df508a0674c3d5
Filip Luchianov <filip.luchianov@signanthealth.com>
2020-01-16 20:30:09 +0200

    132

--- AddCars/cars/car132.py
+++ AddCars/cars/car132.py
@@ -5,41 +5,34 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "peugeot 5008,1.6 hdi,Garantie,CAR PASS,euro 5"
+var_title = "citroen xsara 1.6 benzine,Garantie,CAR PASS,euro 4"
 var_categorie = "Auto's"
-var_brand = "Peugeot"
-var_model = "5008"
-var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n\n!!!!navigatie scherm is kapot !!!\nherstellingskosten +/-300 euro\n\n"
-var_price = "6999"
+var_brand = "Citroën"
+var_model = "Xsara"
+var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "1999"
 var_picspath = "car132"
 var_carroserie = ""
 var_month = ""
-var_year = "2013"
-var_gas = "Diesel"
+var_year = "2002"
+var_gas = "Benzine"
 var_transmissie = "Hand"
-var_km = "107000"
+var_km = "114000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1560"
-var_co2 = "128"
-var_euro = "Euro 5"
-var_options = """Bluetooth
-Elektrische buitenspiegels
-Parkeersensor
-USB
-Elektrische ramen
-Panoramadak
-Isofix
-Alarm
-Lichtmetalen velgen
-Boordcomputer
-Climate control
+var_cilinder = "1587"
+var_co2 = "164"
+var_euro = "euro 4"
+var_options = """Boordcomputer
 Centrale vergrendeling
 Traction-control
-Verwarmde buitenspiegels
+Elektrische ramen
 ABS
-Mistlampen
+Electronic Stability Program (ESP)
 Airbags
-Radio"""
+Isofix
+Alarm
+Radio
+Metaalkleur"""
 var_carcolor = ""
 var_interiorcolor = ""

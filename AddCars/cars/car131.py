ad517af30d36888eba1a3c3d7b9f6e86b36240e8
Filip Luchianov <filip.luchianov@signanthealth.com>
2020-01-28 13:19:11 +0200

    131

--- AddCars/cars/car131.py
+++ AddCars/cars/car131.py
@@ -5,34 +5,34 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "renault clio 1.2 benzine,Garantie,CAR PASS,euro 5"
+var_title = "ford transit 2.2 tdci,CAR PASS,bwj 2011,airco,€ 3800+btw/tva"
 var_categorie = "Auto's"
-var_brand = "Renault"
-var_model = "Clio"
-var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "4999"
+var_brand = "Ford"
+var_model = "Transit"
+var_desc = "\n€ 3800+btw/tva\nexport of handelaar\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "4598"
 var_picspath = "car131"
 var_carroserie = ""
 var_month = ""
-var_year = "2013"
-var_gas = "Benzine"
+var_year = "2011"
+var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "54000"
+var_km = "192000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1149"
-var_co2 = "135"
-var_euro = "Euro 5"
+var_cilinder = "2198"
+var_co2 = ""
+var_euro = "Euro 4"
 var_options = """Boordcomputer
 Centrale vergrendeling
-Elektrische buitenspiegels
+Airconditioning
 Traction-control
 Elektrische ramen
 ABS
+Electronic Stability Program (ESP)
 Airbags
-Isofix
 Alarm
 Radio
-Trekhaak"""
+Metaalkleur"""
 var_carcolor = ""
 var_interiorcolor = ""

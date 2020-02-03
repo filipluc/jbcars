0fa0bf08fa3c7f97429ef9dca070cd11f29e13ea
Filip Luchianov <filip.luchianov@signanthealth.com>
2020-02-03 17:08:43 +0200

    132 134

--- AddCars/cars/car132.py
+++ AddCars/cars/car132.py
@@ -5,34 +5,33 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "citroen xsara 1.6 benzine,Garantie,CAR PASS,euro 4"
+var_title = "peugeot 208,benzine 1.0 cc,Garantie,CAR PASS,euro 6b"
 var_categorie = "Auto's"
-var_brand = "Citroën"
-var_model = "Xsara"
-var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "1999"
+var_brand = "Peugeot"
+var_model = "208"
+var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\nbwj.10/2015\n"
+var_price = "5999"
 var_picspath = "car132"
 var_carroserie = ""
 var_month = ""
-var_year = "2002"
+var_year = "2015"
 var_gas = "Benzine"
 var_transmissie = "Hand"
-var_km = "114000"
+var_km = "98000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1587"
-var_co2 = "164"
-var_euro = "euro 4"
-var_options = """Boordcomputer
+var_cilinder = "999"
+var_co2 = "102"
+var_euro = "euro 6"
+var_options = """Cruise Control
+Boordcomputer
 Centrale vergrendeling
 Traction-control
 Elektrische ramen
 ABS
-Electronic Stability Program (ESP)
 Airbags
 Isofix
 Alarm
-Radio
-Metaalkleur"""
+Radio"""
 var_carcolor = ""
 var_interiorcolor = ""

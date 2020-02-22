3d6d132a05f7a63bd16d5ff4ed409b10eaae0140
Filip Luchianov <filip.luchianov@signanthealth.com>
2020-02-22 10:10:01 +0200

    118 121

--- AddCars/cars/car118.py
+++ AddCars/cars/car118.py
@@ -5,34 +5,40 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "peugeot 2008,1.2 benzine,Garantie,CAR PASS,euro 6b"
+var_title = "vw tiguan 2.0 tdi,4Motion,Garantie,CAR PASS,eerste eigenaar"
 var_categorie = "Auto's"
-var_brand = "Peugeot"
-var_model = "2008"
+var_brand = "Volkswagen"
+var_model = "Tiguan"
 var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "9995"
+var_price = "7950"
 var_picspath = "car118"
 var_carroserie = ""
 var_month = ""
-var_year = "2016"
-var_gas = "Benzine"
+var_year = "2008"
+var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "34000"
+var_km = "139000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1199"
-var_co2 = "114"
-var_euro = "Euro 6"
-var_options = """Cruise Control
-Boordcomputer
+var_cilinder = "1968"
+var_co2 = "182"
+var_euro = "Euro 4"
+var_options = """Boordcomputer
+Climate control
 Centrale vergrendeling
 Elektrische buitenspiegels
+Traction-control
+Parkeersensor
 Verwarmde buitenspiegels
 Elektrische ramen
+4x4
 ABS
+Mistlampen
 Airbags
 Isofix
 Alarm
-Radio"""
+Radio
+Lichtmetalen velgen
+Trekhaak"""
 var_carcolor = ""
 var_interiorcolor = ""
--- AddCars/cars/car121.py
+++ AddCars/cars/car121.py
@@ -5,42 +5,40 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "audi a4,2.0 benzine,AUTOMAAT,euro 5,Garantie,CAR PASS"
+var_title = "renault clio 0,9 benzine,Garantie,CAR PASS,bwj 2015/euro 5b"
 var_categorie = "Auto's"
-var_brand = "Audi"
-var_model = "A4"
+var_brand = "Renault"
+var_model = "Clio"
 var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "9950"
+var_price = "6950"
 var_picspath = "car121"
 var_carroserie = ""
 var_month = ""
-var_year = "2011"
+var_year = "2015"
 var_gas = "Benzine"
-var_transmissie = "Auto"
-var_km = "150000"
-var_doors = "4"
+var_transmissie = "Hand"
+var_km = "104000"
+var_doors = ""
 var_pk = ""
-var_cilinder = "1984"
-var_co2 = "167"
+var_cilinder = "898"
+var_co2 = "104"
 var_euro = "euro 5"
 var_options = """Cruise Control
-Elektrische buitenspiegels
-Elektrische ramen
-Electronic Stability Program (ESP)
-Isofix
-Alarm
-Lichtmetalen velgen
-Metaalkleur
+Bluetooth
 Boordcomputer
-Climate control
 Centrale vergrendeling
+Navigatiesysteem
+Elektrische buitenspiegels
 Airconditioning
 Traction-control
 Verwarmde buitenspiegels
+USB
+Elektrische ramen
 ABS
-Mistlampen
+Electronic Stability Program (ESP)
 Airbags
-Zetelverwarming
+Isofix
+Alarm
 Radio"""
 var_carcolor = ""
 var_interiorcolor = ""

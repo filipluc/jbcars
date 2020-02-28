b93bac7b7478dcc7286ad589dc4d6bb00afb4bc6
Filip Luchianov <filip.luchianov@signanthealth.com>
2020-02-28 15:30:22 +0200

    126

--- AddCars/cars/car126.py
+++ AddCars/cars/car126.py
@@ -5,36 +5,43 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "peugeot partner 1.6 hdi,Garantie,CAR PASS,euro 6b"
+var_title = "citroen c4 PICASSO 1.6 hdi,Garantie,CAR PASS,euro 5"
 var_categorie = "Auto's"
-var_brand = "Peugeot"
-var_model = "Partner"
+var_brand = "Citroën"
+var_model = "C4"
 var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "9999"
+var_price = "4999"
 var_picspath = "car126"
 var_carroserie = "Monovolume"
 var_month = ""
-var_year = "2016"
+var_year = "2012"
 var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "61000"
+var_km = "150000"
 var_doors = ""
 var_pk = ""
 var_cilinder = "1560"
-var_co2 = "113"
-var_euro = "euro 6"
+var_co2 = "132"
+var_euro = "euro 5"
 var_options = """Cruise Control
-Boordcomputer
-Centrale vergrendeling
+Navigatiesysteem
 Elektrische buitenspiegels
+Parkeersensor
+Elektrische ramen
+Electronic Stability Program (ESP)
+Isofix
+Alarm
+Lichtmetalen velgen
+Metaalkleur
+Boordcomputer
+Climate control
+Centrale vergrendeling
+Airconditioning
 Traction-control
 Verwarmde buitenspiegels
-Elektrische ramen
 ABS
 Mistlampen
 Airbags
-Isofix
-Alarm
 Radio"""
 var_carcolor = ""
 var_interiorcolor = ""

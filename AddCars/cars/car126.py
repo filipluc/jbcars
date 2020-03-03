11681d07deee9d6c2d8426f4fed77a235539b15e
Filip Luchianov <filip.luchianov@signanthealth.com>
2020-03-03 15:20:56 +0200

    126

--- AddCars/cars/car126.py
+++ AddCars/cars/car126.py
@@ -5,43 +5,56 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "citroen c4 PICASSO 1.6 hdi,Garantie,CAR PASS,euro 5"
+var_title = "peugeot 508 RXH,2.0 hdi,Garantie,CAR PASS,46000 km/bwj 2016"
 var_categorie = "Auto's"
-var_brand = "Citroën"
-var_model = "C4"
+var_brand = "Peugeot"
+var_model = "508"
 var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "4999"
+var_price = "19995"
 var_picspath = "car126"
-var_carroserie = "Monovolume"
+var_carroserie = ""
 var_month = ""
-var_year = "2012"
+var_year = "2016"
 var_gas = "Diesel"
-var_transmissie = "Hand"
-var_km = "150000"
+var_transmissie = "Auto"
+var_km = "46000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1560"
-var_co2 = "132"
+var_cilinder = "1997"
+var_co2 = "119"
 var_euro = "euro 5"
 var_options = """Cruise Control
+Startonderbreker
+Bluetooth
+Parkeercamera
+Adaptive Cruise Control
 Navigatiesysteem
 Elektrische buitenspiegels
+Elektrische stoelverstelling
 Parkeersensor
+USB
 Elektrische ramen
+Panoramadak
 Electronic Stability Program (ESP)
+Adaptieve lichten
 Isofix
 Alarm
 Lichtmetalen velgen
+Keyless entry
+Elektrische achterklep
 Metaalkleur
+Lederen bekleding
 Boordcomputer
 Climate control
 Centrale vergrendeling
 Airconditioning
+Start-stop-systeem
 Traction-control
 Verwarmde buitenspiegels
 ABS
 Mistlampen
 Airbags
+Zetelverwarming
 Radio"""
 var_carcolor = ""
 var_interiorcolor = ""

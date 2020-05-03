d88e47278e3bc128d465b3c145a09ee19a969b7c
Filip Luchianov <filip.luchianov@signanthealth.com>
2020-05-03 14:23:38 +0300

    remove login

--- AddCars/addcar_single.py
+++ AddCars/addcar_single.py
@@ -20,40 +20,44 @@ from AddCars.cars import car104
 
 
 
-
 class AdaugaMasiniSingle(unittest.TestCase):
 
     def setUp(self):
         options = webdriver.ChromeOptions()
-        options.add_argument("--start-maximized")
+        # options.add_argument("--start-maximized")
         #options.add_argument("--incognito")
-        self.driver = webdriver.Chrome(chrome_options=options)
-        
+        options.add_experimental_option("debuggerAddress", "127.0.0.1:1111")
+        # self.driver = webdriver.Chrome(chrome_options=options)
+        self.driver = webdriver.Chrome(executable_path='C:\SeleniumDrivers\chromedriver.exe', chrome_options=options)
         driver = self.driver
-        #driver.implicitly_wait(5)
-        driver.get("https://www.2dehands.be/login.html")
-        self.assertIn("2dehands", driver.title, "Page was not opened correctly")
-        elem1 = driver.find_element_by_name("j_username")
-        elem1.clear()
-        # elem1.send_keys("ljale@hotmail.com")
-        elem1.send_keys("jb.cars@hotmail.com")
-        elem2 = driver.find_element_by_name("j_password")
-        elem2.clear()
-        # elem2.send_keys("Boni/1994")
-        elem2.send_keys("103robel")
-        elem2.send_keys(Keys.RETURN)
-        
-        #using explicit wait with assert
-        try:
-            WebDriverWait(driver, 5).until(EC.title_contains("Tweedehands"), "Login was not successfull")
-        except TimeoutException:
-            self.assertIn("Tweedehands", driver.title, "Login was not successfull")
-        finally:
-            #driver.quit()
-            pass
+
+        # #driver.implicitly_wait(5)
+        # driver.get("https://www.2dehands.be/login.html")
+        # #driver.get("https://www.2dehands.be")
+        # self.assertIn("2dehands", driver.title, "Page was not opened correctly")
+        # elem1 = driver.find_element_by_name("j_username")
+        # elem1.clear()
+        # # elem1.send_keys("ljale@hotmail.com")
+        # elem1.send_keys("jb.cars@hotmail.com")
+        # elem2 = driver.find_element_by_name("j_password")
+        # elem2.clear()
+        # # elem2.send_keys("Boni/1994")
+        # elem2.send_keys("103robel")
+        # elem2.send_keys(Keys.RETURN)
+        # #time.sleep(600)
+        #
+        # #using explicit wait with assert
+        # try:
+        #     WebDriverWait(driver, 5).until(EC.title_contains("Tweedehands"), "Login was not successfull")
+        # except TimeoutException:
+        #     self.assertIn("Tweedehands", driver.title, "Login was not successfull")
+        # finally:
+        #     #driver.quit()
+        pass
         
     def tearDown(self):
-        self.driver.quit()
+        # self.driver.quit()
+        pass
 
     #@unittest.skip("skip car") 
     def test_car104(self):

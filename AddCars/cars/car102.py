4491623e9691facc524d276bfb0821eb7da65ef5
Filip Luchianov <filip.luchianov@signanthealth.com>
2020-05-07 12:26:36 +0300

    102

--- AddCars/cars/car102.py
+++ AddCars/cars/car102.py
@@ -5,45 +5,42 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "skoda octavia break 1.9 tdi,Garantie,CAR PASS,leder,navigati"
+var_title = "hyundai i40 break,1.7 crdi,Garantie,CAR PASS,euro 5"
 var_categorie = "Auto's"
-var_brand = "Skoda"
-var_model = "Octavia"
+var_brand = "Hyundai"
+var_model = "i40"
 var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "3999"
+var_price = "6900"
 var_picspath = "car102"
 var_carroserie = "Break"
 var_month = ""
-var_year = "2008"
+var_year = "2012"
 var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "227000"
-var_doors = "5"
+var_km = "142000"
+var_doors = ""
 var_pk = ""
-var_cilinder = "1896"
-var_co2 = "137"
-var_euro = "euro 4"
-var_options = """Cruise Control
-Bluetooth
-Navigatiesysteem
+var_cilinder = "1685"
+var_co2 = "122"
+var_euro = "euro 5"
+var_options = """Alarm
 Elektrische buitenspiegels
-Parkeersensor
-Elektrische ramen
-Electronic Stability Program (ESP)
-Adaptieve lichten
-Isofix
-Alarm
-Lichtmetalen velgen
-Metaalkleur
-Lederen bekleding
-Boordcomputer
 Climate control
+Mistlampen
+Radio
+Traction-control
+Verwarmde buitenspiegels
+ABS
+Airbags
+Boordcomputer
 Centrale vergrendeling
 Airconditioning
-Traction-control
-ABS
-Mistlampen
-Airbags
-Radio"""
+Elektrische ramen
+Electronic Stability Program (ESP)
+Isofix
+Lichtmetalen velgen
+Metaalkleur
+Parkeersensor
+Cruise Control"""
 var_carcolor = ""
 var_interiorcolor = ""

0cc55b50ace63837071b38243ab34261ba09b29e
Filip Luchianov <filip.luchianov@signanthealth.com>
2020-05-11 14:40:02 +0300

    remove 108

--- AddCars/addcarTOATE.py
+++ AddCars/addcarTOATE.py
@@ -109,8 +109,8 @@ class AdaugaMasini(unittest.TestCase):
     def test_car107(self):
         GeneralFunctions.deleteAddCarFunction(self, car107)
     
-    def test_car108(self):
-        GeneralFunctions.deleteAddCarFunction(self, car108)
+    # def test_car108(self):
+    #     GeneralFunctions.deleteAddCarFunction(self, car108)
     
     def test_car109(self):
         GeneralFunctions.deleteAddCarFunction(self, car109)

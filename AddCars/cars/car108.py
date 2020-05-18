8d3286a957b53809c2bcaa259a7a74d3af76db33
Filip Luchianov <filip.luchianov@signanthealth.com>
2020-05-18 12:53:26 +0300

    108

--- AddCars/cars/car108.py
+++ AddCars/cars/car108.py
@@ -5,27 +5,36 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "vw fox 1.2 benzine ,Garantie"
+var_title = "vw polo 1.4 tdi,Garantie,CAR PASS"
 var_categorie = "Auto's"
 var_brand = "Volkswagen"
-var_model = "Fox"
+var_model = "Polo"
 var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "2500"
+var_price = "2999"
 var_picspath = "car108"
-var_carroserie = "Stadsauto"
-var_month = "feb"
-var_year = "2006"
-var_gas = "Benzine"
+var_carroserie = ""
+var_month = ""
+var_year = "2007"
+var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "135000"
-var_doors = "3"
-var_pk = "55"
-var_cilinder = "1198"
-var_co2 = "146"
+var_km = "150000"
+var_doors = ""
+var_pk = ""
+var_cilinder = "1422"
+var_co2 = "119"
 var_euro = "euro 4"
-var_options = """Boordcomputer
+var_options = """Centrale vergrendeling
+Airconditioning
+Elektrische ramen
+Electronic Stability Program (ESP)
+Isofix
+Metaalkleur
+Alarm
+Elektrische buitenspiegels
+Radio
+Traction-control
 ABS
 Airbags
-Radio"""
-var_carcolor = "Geel"
-var_interiorcolor = "Grijs"
+Boordcomputer"""
+var_carcolor = ""
+var_interiorcolor = ""

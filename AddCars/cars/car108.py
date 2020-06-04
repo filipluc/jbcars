a372f7d682c7c44e04a9a39e24a5c1f1816056d8
Filip Luchianov <filip.luchianov@signanthealth.com>
2020-06-04 17:29:21 +0300

    108

--- AddCars/cars/car108.py
+++ AddCars/cars/car108.py
@@ -5,36 +5,39 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "vw polo 1.4 tdi,Garantie,CAR PASS"
+var_title = "citroen c3,1.4 hdi,Garantie,CAR PASS"
 var_categorie = "Auto's"
-var_brand = "Volkswagen"
-var_model = "Polo"
+var_brand = "Citroën"
+var_model = "C3"
 var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
 var_price = "2999"
 var_picspath = "car108"
 var_carroserie = ""
 var_month = ""
-var_year = "2007"
+var_year = "2006"
 var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "150000"
+var_km = "140000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1422"
-var_co2 = "119"
+var_cilinder = "1398"
+var_co2 = "115"
 var_euro = "euro 4"
-var_options = """Centrale vergrendeling
+var_options = """Alarm
+Elektrische buitenspiegels
+Climate control
+Radio
+Traction-control
+Verwarmde buitenspiegels
+ABS
+Airbags
+Boordcomputer
+Centrale vergrendeling
 Airconditioning
 Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
 Metaalkleur
-Alarm
-Elektrische buitenspiegels
-Radio
-Traction-control
-ABS
-Airbags
-Boordcomputer"""
+Cruise Control"""
 var_carcolor = ""
 var_interiorcolor = ""

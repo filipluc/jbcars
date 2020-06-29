ff561b8fc3e478483bd8019a9a6e8e20ce58702c
Filip Luchianov <filip.luchianov@signanthealth.com>
2020-06-29 16:54:11 +0300

    111

--- AddCars/cars/car111.py
+++ AddCars/cars/car111.py
@@ -5,32 +5,50 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "peugeot partner 2.0 hdi,LICHTE VRACHT,2 zit"
+var_title = "peugeot 508 sw,1.6 hdi AUTOMAAT,garantie,CAR PASS,bwj.2015"
 var_categorie = "Auto's"
 var_brand = "Peugeot"
-var_model = "Partner"
-var_desc = "\nexport of handelaar\n\n"
-var_price = "1500"
+var_model = "508"
+var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "8900"
 var_picspath = "car111"
 var_carroserie = ""
 var_month = ""
-var_year = "2004"
+var_year = "2015"
 var_gas = "Diesel"
-var_transmissie = "Hand"
-var_km = "219000"
+var_transmissie = "Auto"
+var_km = "144000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1997"
-var_co2 = ""
-var_euro = "euro 3"
-var_options = """Boordcomputer
-Centrale vergrendeling
-Traction-control
-Elektrische ramen
-ABS
+var_cilinder = "1560"
+var_co2 = "105"
+var_euro = "euro 5"
+var_options = """ABS
 Airbags
+Lederen bekleding
+Boordcomputer
+Centrale vergrendeling
+Airconditioning
+Elektrische ramen
+Electronic Stability Program (ESP)
+Isofix
+Lichtmetalen velgen
+Metaalkleur
+Navigatiesysteem
+Parkeersensor
+Trekhaak
+Cruise Control
+Climate control
 Alarm
+Bluetooth
+Parkeercamera
+Elektrische buitenspiegels
+Mistlampen
 Radio
-Trekhaak"""
+Start-stop-systeem
+Traction-control
+Verwarmde buitenspiegels
+Panoramadak
+Keyless entry"""
 var_carcolor = ""
 var_interiorcolor = ""

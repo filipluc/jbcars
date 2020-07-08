99b81bc994d7a8465eed795982fd3b10d929dc1e
Filip Luchianov <filip.luchianov@signanthealth.com>
2020-07-08 10:01:52 +0300

    105

--- AddCars/cars/car105.py
+++ AddCars/cars/car105.py
@@ -5,38 +5,42 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "suzuki sx4,1.5 benzine,Garantie,CAR PASS"
+var_title = "peugeot partner OUTDOOR 1.6 hdi,garantie,CAR PASS,euro 5"
 var_categorie = "Auto's"
-var_brand = "Suzuki"
-var_model = "SX4"
+var_brand = "Peugeot"
+var_model = "Partner"
 var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "3900"
+var_price = "5650"
 var_picspath = "car105"
 var_carroserie = ""
 var_month = ""
-var_year = "2009"
-var_gas = "Benzine"
+var_year = "2012"
+var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "167000"
+var_km = "189000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1490"
-var_co2 = "159"
-var_euro = "euro 4"
-var_options = """Boordcomputer
+var_cilinder = "1560"
+var_co2 = "139"
+var_euro = "euro 5"
+var_options = """ABS
+Airbags
+Boordcomputer
 Centrale vergrendeling
-Elektrische buitenspiegels
 Airconditioning
+Elektrische ramen
+Electronic Stability Program (ESP)
+Isofix
+Parkeersensor
+Trekhaak
+Climate control
+Alarm
+Elektrische buitenspiegels
+Mistlampen
+Radio
 Traction-control
 Verwarmde buitenspiegels
-Elektrische ramen
-ABS
-Electronic Stability Program (ESP)
-Airbags
-Isofix
-Alarm
-Radio
-Trekhaak
-Metaalkleur"""
+Schuifdeur
+Stuurwielverwarming"""
 var_carcolor = ""
 var_interiorcolor = ""

02e5cc0b3c50f7b46f86373877f6b9ead61705ee
Filip Luchianov <filip.luchianov@signanthealth.com>
2020-07-13 16:03:51 +0300

    105

--- AddCars/cars/car105.py
+++ AddCars/cars/car105.py
@@ -5,42 +5,31 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "peugeot partner OUTDOOR 1.6 hdi,garantie,CAR PASS,euro 5"
+var_title = "citroen c3, 1.1 benzine,Garantie,CAR PASS,55000 km"
 var_categorie = "Auto's"
-var_brand = "Peugeot"
-var_model = "Partner"
+var_brand = "Citroën"
+var_model = "C3"
 var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "5650"
+var_price = "3990"
 var_picspath = "car105"
 var_carroserie = ""
 var_month = ""
-var_year = "2012"
-var_gas = "Diesel"
+var_year = "2008"
+var_gas = "Benzine"
 var_transmissie = "Hand"
-var_km = "189000"
+var_km = "55000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1560"
-var_co2 = "139"
-var_euro = "euro 5"
+var_cilinder = "1124"
+var_co2 = "140"
+var_euro = "euro 4"
 var_options = """ABS
 Airbags
 Boordcomputer
-Centrale vergrendeling
-Airconditioning
 Elektrische ramen
 Electronic Stability Program (ESP)
-Isofix
-Parkeersensor
-Trekhaak
-Climate control
-Alarm
-Elektrische buitenspiegels
-Mistlampen
+Metaalkleur
 Radio
-Traction-control
-Verwarmde buitenspiegels
-Schuifdeur
-Stuurwielverwarming"""
+Traction-control"""
 var_carcolor = ""
 var_interiorcolor = ""

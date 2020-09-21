2bfe4850d95454eee39e5ee2441e9296bf57b679
Filip Luchianov <filip.luchianov@signanthealth.com>
2020-09-21 11:20:21 +0300

    101

--- AddCars/cars/car101.py
+++ AddCars/cars/car101.py
@@ -5,36 +5,50 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "peugeot 207,1.6 hdi,Garantie,CAR PASS"
+var_title = "peugeot 308 GT line 1.6 hdi,Garantie,CAR PASS,euro 5b"
 var_categorie = "Auto's"
 var_brand = "Peugeot"
-var_model = "207"
-var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "3999"
+var_model = "308"
+var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n\ndistributie riem+water pomp juist vervangen\n"
+var_price = "12900"
 var_picspath = "car101"
 var_carroserie = ""
 var_month = ""
-var_year = "2010"
+var_year = "2015"
 var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "111000"
+var_km = "99000"
 var_doors = ""
 var_pk = ""
 var_cilinder = "1560"
 var_co2 = ""
-var_euro = "euro 4"
-var_options = """Airbags
+var_euro = "euro 5"
+var_options = """ABS
+Airbags
+Lederen bekleding
 Boordcomputer
 Centrale vergrendeling
 Airconditioning
 Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
+Lichtmetalen velgen
 Metaalkleur
+Navigatiesysteem
+Parkeersensor
+Sportstoelen
+Cruise Control
+Climate control
 Alarm
+Bluetooth
+Parkeercamera
 Elektrische buitenspiegels
+Mistlampen
 Radio
+Sportpakket
 Traction-control
-Verwarmde buitenspiegels"""
+Verwarmde buitenspiegels
+USB
+Panoramadak"""
 var_carcolor = ""
 var_interiorcolor = ""

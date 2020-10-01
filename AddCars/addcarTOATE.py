5450c31b02fdbe530aa0b01e6df7d0ae0820af6b
Filip Luchianov <filip.luchianov@signanthealth.com>
2020-10-01 19:19:13 +0300

    113 130

--- AddCars/addcarTOATE.py
+++ AddCars/addcarTOATE.py
@@ -124,8 +124,8 @@ class AdaugaMasini(unittest.TestCase):
     def test_car112(self):
         GeneralFunctions.deleteAddCarFunction(self, car112)
         
-    # def test_car113(self):
-    #     GeneralFunctions.deleteAddCarFunction(self, car113)
+    def test_car113(self):
+        GeneralFunctions.deleteAddCarFunction(self, car113)
     
     def test_car114(self):
         GeneralFunctions.deleteAddCarFunction(self, car114)
@@ -154,8 +154,8 @@ class AdaugaMasini(unittest.TestCase):
     def test_car122(self):
         GeneralFunctions.deleteAddCarFunction(self, car122)
 
-    def test_car123(self):
-        GeneralFunctions.deleteAddCarFunction(self, car123)
+    # def test_car123(self):
+    #     GeneralFunctions.deleteAddCarFunction(self, car123)
 
     # def test_car124(self):
     #     GeneralFunctions.deleteAddCarFunction(self, car124)
--- AddCars/cars/car113.py
+++ AddCars/cars/car113.py
@@ -5,31 +5,25 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "Citroen C3 ,1.4i,Garantie,CAR PASS,73000 km"
+var_title = "dacia logan break 1.4 benzine,Garantie,CAR PASS,70000 km"
 var_categorie = "Auto's"
-var_brand = "Citroën"
-var_model = "C3"
-var_desc = "\ndak geblokeerd/toit bloque\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "2999"
+var_brand = "Dacia"
+var_model = "Logan"
+var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "3500"
 var_picspath = "car113"
 var_carroserie = ""
 var_month = ""
-var_year = "2008"
+var_year = "2010"
 var_gas = "Benzine"
 var_transmissie = "Hand"
-var_km = "73000"
+var_km = "70000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1360"
-var_co2 = "163"
+var_cilinder = "1390"
+var_co2 = ""
 var_euro = "euro 4"
-var_options = """Boordcomputer
-Centrale vergrendeling
-Traction-control
-Elektrische ramen
-ABS
-Airbags
-Alarm
+var_options = """ABS
 Radio"""
 var_carcolor = ""
 var_interiorcolor = ""

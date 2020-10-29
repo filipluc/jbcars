0c344a011efe4b5d2f244835fae57925343334a3
Filip Luchianov <filip.luchianov@signanthealth.com>
2020-10-29 12:09:05 +0200

    102

--- AddCars/cars/car102.py
+++ AddCars/cars/car102.py
@@ -5,25 +5,32 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "ford focus 1.6 tdci,Garantie,CAR PASS"
+var_title = "bmw 316d,CAR PASS,garantie,euro 5"
 var_categorie = "Auto's"
-var_brand = "Ford"
-var_model = "Focus"
+var_brand = "BMW"
+var_model = "3 Reeks"
 var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "3850"
+var_price = "5999"
 var_picspath = "car102"
 var_carroserie = "Break"
 var_month = ""
-var_year = "2009"
+var_year = "2010"
 var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "188000"
+var_km = "171000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1560"
-var_co2 = "114"
-var_euro = "euro 4"
-var_options = """ABS
+var_cilinder = "1995"
+var_co2 = "119"
+var_euro = "euro 5"
+var_options = """Alarm
+Elektrische buitenspiegels
+Climate control
+Mistlampen
+Radio
+Start-stop-systeem
+Traction-control
+ABS
 Airbags
 Lederen bekleding
 Boordcomputer
@@ -34,12 +41,8 @@ Electronic Stability Program (ESP)
 Isofix
 Lichtmetalen velgen
 Metaalkleur
-Cruise Control
-Alarm
-Elektrische buitenspiegels
-Mistlampen
-Radio
-Traction-control
-Verwarmde buitenspiegels"""
+Parkeersensor
+Trekhaak
+Cruise Control"""
 var_carcolor = ""
 var_interiorcolor = ""

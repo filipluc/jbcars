9ff1f1cdafa2a02b3e64c5d741066b5df5dc4f3c
Filip Luchianov <filip.luchianov@signanthealth.com>
2020-11-03 10:45:39 +0200

    120

--- AddCars/cars/car120.py
+++ AddCars/cars/car120.py
@@ -5,36 +5,29 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "peugeot partner 1.6d,Garantie,CAR PASS,5700€ btw/tva,euro 5"
+var_title = "citroen jumper 2.2hdi, CAR PASS,gekeurd,€ 4124 excl btw/tva"
 var_categorie = "Auto's"
-var_brand = "Peugeot"
-var_model = "Partner"
-var_desc = "\nprijs 5700€+btw/tva\n\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "6823"
+var_brand = "Bestelwagens en Lichte vracht"
+var_model = "Citroën"
+var_desc = "\nprijs 4124 euro + btw/tva\n\enkel voor handelaar of export\njuist nieuwe remschijf x4 en nieuwe remblokken\ndistributie op ketting\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "4999"
 var_picspath = "car120"
 var_carroserie = ""
 var_month = ""
-var_year = "2013"
+var_year = "2007"
 var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "114000"
+var_km = "178000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1560"
-var_co2 = "135"
-var_euro = "euro 5"
-var_options = """ABS
-Boordcomputer
-Centrale vergrendeling
-Airconditioning
+var_cilinder = "2198"
+var_co2 = ""
+var_euro = "euro 4"
+var_options = """Centrale vergrendeling
 Elektrische ramen
 Electronic Stability Program (ESP)
-Isofix
-Metaalkleur
-Alarm
-Elektrische buitenspiegels
-Radio
-Traction-control
-Schuifdeur"""
+ABS
+Airbags
+Boordcomputer"""
 var_carcolor = ""
 var_interiorcolor = ""

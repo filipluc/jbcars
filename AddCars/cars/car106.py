17c21d76288e581a879dce10630d1eee65334d42
Filip Luchianov <filip.luchianov@signanthealth.com>
2020-11-09 09:17:24 +0200

    106 116

--- AddCars/cars/car106.py
+++ AddCars/cars/car106.py
@@ -5,40 +5,45 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "peugeot partner 1.6 hdi AUTOMAAT,garantie,CAR PASS,euro 5b"
+var_title = "citroen c3 EXCLUSIVE 1.2 benzine,Garantie,CAR PASS,euro 5b"
 var_categorie = "Auto's"
-var_brand = "Peugeot"
-var_model = "Partner"
-var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n\nprijs 8760€+btw/tva\n"
-var_price = "10600"
+var_brand = "Citroën"
+var_model = "C3"
+var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "6950"
 var_picspath = "car106"
 var_carroserie = ""
 var_month = ""
-var_year = "2015"
-var_gas = "Diesel"
-var_transmissie = "Auto"
-var_km = "57000"
+var_year = "2014"
+var_gas = "Benzine"
+var_transmissie = "Hand"
+var_km = "67000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1560"
-var_co2 = "115"
+var_cilinder = "1199"
+var_co2 = "107"
 var_euro = "euro 5"
-var_options = """ABS
-Airbags
-Boordcomputer
-Centrale vergrendeling
-Airconditioning
-Elektrische ramen
-Electronic Stability Program (ESP)
-Isofix
-Metaalkleur
-Parkeersensor
-Alarm
+var_options = """Alarm
+Bluetooth
 Elektrische buitenspiegels
+Climate control
 Mistlampen
 Radio
 Traction-control
 Verwarmde buitenspiegels
-Schuifdeur"""
+ABS
+Airbags
+Boordcomputer
+Centrale vergrendeling
+Airconditioning
+Panoramadak
+Elektrische ramen
+Electronic Stability Program (ESP)
+Isofix
+Lichtmetalen velgen
+Metaalkleur
+Parkeersensor
+Trekhaak
+Cruise Control"""
 var_carcolor = ""
 var_interiorcolor = ""
--- AddCars/cars/car116.py
+++ AddCars/cars/car116.py
@@ -5,38 +5,28 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "peugeot partner 1.6 benzine,Garantie,CAR PASS"
+var_title = "toyota aygo 1.0 benzine,Garantie,CAR PASS,79000 km"
 var_categorie = "Auto's"
-var_brand = "Peugeot"
-var_model = "Partner"
-var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n\ndistributie riem vervangen op 212000 km\n"
-var_price = "3999"
+var_brand = "Toyota"
+var_model = "Aygo"
+var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "2999"
 var_picspath = "car116"
-var_carroserie = "Monovolume"
+var_carroserie = ""
 var_month = ""
-var_year = "2009"
+var_year = "2008"
 var_gas = "Benzine"
 var_transmissie = "Hand"
-var_km = "236000"
+var_km = "79000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1587"
-var_co2 = ""
+var_cilinder = "998"
+var_co2 = "109"
 var_euro = "euro 4"
-var_options = """ABS
-Airbags
-Boordcomputer
-Centrale vergrendeling
-Airconditioning
-Elektrische ramen
+var_options = """Elektrische ramen
 Electronic Stability Program (ESP)
-Isofix
 Metaalkleur
-Trekhaak
-Alarm
-Elektrische buitenspiegels
 Radio
-Traction-control
-Schuifdeur"""
+Airbags"""
 var_carcolor = ""
 var_interiorcolor = ""

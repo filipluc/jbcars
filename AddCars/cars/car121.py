c325e89e6116dbee9d85eadbd756c37b41987115
Filip Luchianov <filip.luchianov@signanthealth.com>
2020-11-18 11:56:43 +0200

    121

--- AddCars/cars/car121.py
+++ AddCars/cars/car121.py
@@ -5,41 +5,40 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "volvo v40 D2,1.6 d,Garantie,CAR PASS,euro 5"
+var_title = "peugeot 208 ,1.4hdi,Garantie,CAR PASS,€4950 excl btw/tva"
 var_categorie = "Auto's"
-var_brand = "Volvo"
-var_model = "V40"
+var_brand = "Peugeot"
+var_model = "208"
 var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "6500"
+var_price = "5990"
 var_picspath = "car121"
 var_carroserie = ""
 var_month = ""
 var_year = "2012"
 var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "189000"
+var_km = "152000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1560"
-var_co2 = "94"
+var_cilinder = "1398"
+var_co2 = "98"
 var_euro = "euro 5"
-var_options = """ABS
+var_options = """Alarm
+Bluetooth
+Elektrische buitenspiegels
+Radio
+Traction-control
+Verwarmde buitenspiegels
+ABS
 Airbags
 Boordcomputer
 Centrale vergrendeling
+USB
 Airconditioning
 Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
-Lichtmetalen velgen
 Metaalkleur
-Navigatiesysteem
-Cruise Control
-Climate control
-Alarm
-Elektrische buitenspiegels
-Radio
-Traction-control
-Verwarmde buitenspiegels"""
+Cruise Control"""
 var_carcolor = ""
 var_interiorcolor = ""

afb6e2cf909e127f6eb6e17524d473885613c34e
Filip Luchianov <filip.luchianov@signanthealth.com>
2021-01-11 16:57:06 +0200

    123

--- AddCars/cars/car123.py
+++ AddCars/cars/car123.py
@@ -5,26 +5,38 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "vw lupo 1.0 i benzine,Garantie,CAR PASS"
+var_title = "peugeot 206+ plus,1.1 benzine,Garantie,CAR PASS,airco,euro 5"
 var_categorie = "Auto's"
-var_brand = "Volkswagen"
-var_model = "Lupo"
-var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n\nextra 2 stoelen =+450 euro\n"
-var_price = "1999"
+var_brand = "Peugeot"
+var_model = "206"
+var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "4500"
 var_picspath = "car123"
 var_carroserie = ""
 var_month = ""
-var_year = "1999"
+var_year = "2012"
 var_gas = "Benzine"
 var_transmissie = "Hand"
-var_km = "123000"
+var_km = "70000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "997"
-var_co2 = ""
-var_euro = ""
-var_options = """Radio
+var_cilinder = "1124"
+var_co2 = "133"
+var_euro = "Euro 5"
+var_options = """Centrale vergrendeling
+Airconditioning
+Elektrische ramen
+Electronic Stability Program (ESP)
+Isofix
+Metaalkleur
+Alarm
+Elektrische buitenspiegels
+Mistlampen
+Radio
+Traction-control
+Verwarmde buitenspiegels
 ABS
-Airbags"""
+Airbags
+Boordcomputer"""
 var_carcolor = ""
 var_interiorcolor = ""

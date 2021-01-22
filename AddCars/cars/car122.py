faae9be55bb386912c17ceda304ce51d686f981a
Filip Luchianov <filip.luchianov@signanthealth.com>
2021-01-22 09:04:39 +0200

    122

--- AddCars/cars/car122.py
+++ AddCars/cars/car122.py
@@ -5,35 +5,50 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "ford transit 2.2 tdci Lichte Vracht,Bwj 2010,€ 2479 excl btw"
+var_title = "ford kuga 2.0 d,Garantie,CAR PASS,bwj.2014,euro 5b"
 var_categorie = "Auto's"
 var_brand = "Ford"
-var_model = "Transit"
-var_desc = "\nprijs =2479€+btw/tva\nenkel voor export of handelaar\nkeuring = +200 euro\n\n"
-var_price = "3000"
+var_model = "Kuga"
+var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\neerste eigenaar met oh-boekje\n"
+var_price = "10650"
 var_picspath = "car122"
-var_carroserie = ""
+var_carroserie = "SUV"
 var_month = ""
-var_year = "2010"
+var_year = "2014"
 var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "250000"
+var_km = "131000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "2198"
-var_co2 = ""
-var_euro = "euro 4"
-var_options = """Centrale vergrendeling
+var_cilinder = "1997"
+var_co2 = "139"
+var_euro = "euro 5"
+var_options = """Keyless entry
+Alarm
+Bluetooth
+Elektrische buitenspiegels
+Elektrische stoelverstelling
+Climate control
+Mistlampen
+Radio
+Start-stop-systeem
+Traction-control
+Verwarmde buitenspiegels
+Elektrische koffer
+ABS
+Airbags
+Lederen bekleding
+Boordcomputer
+Centrale vergrendeling
 Airconditioning
 Elektrische ramen
 Electronic Stability Program (ESP)
+Isofix
+Lichtmetalen velgen
 Metaalkleur
-Trekhaak
-Alarm
-Radio
-Traction-control
-ABS
-Airbags
-Boordcomputer"""
+Navigatiesysteem
+Parkeersensor
+Zetelverwarming
+Cruise Control"""
 var_carcolor = ""
 var_interiorcolor = ""

c202938d2f05520e6ee354892b78ed7d964bd3b7
Filip Luchianov <filip.luchianov@signanthealth.com>
2021-02-12 11:10:10 +0200

    107

--- AddCars/cars/car107.py
+++ AddCars/cars/car107.py
@@ -5,47 +5,37 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "audi a3 CABRIO,2.0 d,AUTOMAAT,bwj 2015,68000km,euro 6b"
+var_title = "opel corsa 1.4 benzine,Garantie,CAR PASS,euro 5"
 var_categorie = "Auto's"
-var_brand = "Audi"
-var_model = "A3"
+var_brand = "Opel"
+var_model = "Corsa"
 var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "22500"
+var_price = "4500"
 var_picspath = "car107"
-var_carroserie = "Cabriolet"
+var_carroserie = ""
 var_month = ""
-var_year = "2015"
-var_gas = "Diesel"
-var_transmissie = "Auto"
-var_km = "68000"
+var_year = "2010"
+var_gas = "Benzine"
+var_transmissie = "Hand"
+var_km = "83000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1968"
-var_co2 = "120"
-var_euro = "euro 6"
-var_options = """ABS
-Airbags
-Lederen bekleding
-Boordcomputer
-Centrale vergrendeling
-Airconditioning
+var_cilinder = "1398"
+var_co2 = "129"
+var_euro = "euro 5"
+var_options = """Centrale vergrendeling
 Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
 Lichtmetalen velgen
 Metaalkleur
 Navigatiesysteem
-Parkeersensor
-Zetelverwarming
-Climate control
 Alarm
-Bluetooth
 Elektrische buitenspiegels
-Elektrische stoelverstelling
-Mistlampen
 Radio
-Sportpakket
 Traction-control
-Verwarmde buitenspiegels"""
+ABS
+Airbags
+Boordcomputer"""
 var_carcolor = ""
 var_interiorcolor = ""

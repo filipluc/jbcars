a41207123d0aee87b73b3ce2648eb6f60c594881
Filip Luchianov <filip.luchianov@signanthealth.com>
2021-03-01 11:00:37 +0200

    106 107

--- AddCars/cars/car106.py
+++ AddCars/cars/car106.py
@@ -5,45 +5,38 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "citroen c3 EXCLUSIVE 1.2 benzine,Garantie,CAR PASS,euro 5b"
+var_title = "opel vivaro 2.0 cdti,DUBBELE CABINE 5 pl,NAVI,airco,L.V."
 var_categorie = "Auto's"
-var_brand = "Citroën"
-var_model = "C3"
-var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "6950"
+var_brand = "Opel"
+var_model = "Vivaro"
+var_desc = "\nenkel voor export of handelaar\ntrekhaak=2000 kg\nCAR PASS\nwordt gekeurd met roos formulier\n"
+var_price = "5850"
 var_picspath = "car106"
 var_carroserie = ""
 var_month = ""
-var_year = "2014"
-var_gas = "Benzine"
+var_year = "2007"
+var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "67000"
+var_km = "187000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1199"
-var_co2 = "107"
-var_euro = "euro 5"
-var_options = """Alarm
-Bluetooth
-Elektrische buitenspiegels
-Climate control
-Mistlampen
-Radio
-Traction-control
-Verwarmde buitenspiegels
-ABS
-Airbags
-Boordcomputer
-Centrale vergrendeling
+var_cilinder = "1995"
+var_co2 = ""
+var_euro = "euro 4"
+var_options = """Centrale vergrendeling
 Airconditioning
-Panoramadak
 Elektrische ramen
 Electronic Stability Program (ESP)
-Isofix
-Lichtmetalen velgen
 Metaalkleur
-Parkeersensor
+Navigatiesysteem
 Trekhaak
-Cruise Control"""
+Alarm
+Parkeercamera
+Elektrische buitenspiegels
+Radio
+Traction-control
+ABS
+Airbags
+Boordcomputer"""
 var_carcolor = ""
 var_interiorcolor = ""
--- AddCars/cars/car107.py
+++ AddCars/cars/car107.py
@@ -5,37 +5,40 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "opel corsa 1.4 benzine,Garantie,CAR PASS,euro 5"
+var_title = "opel vivaro 2.0 cdti,Lichte vracht,DUBBELE CABINE 5 pl,airco"
 var_categorie = "Auto's"
-var_brand = "Opel"
-var_model = "Corsa"
-var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "4500"
+var_brand = "Bestelwagens en Lichte vracht"
+var_model = "Opel"
+var_desc = "\nenkel voor export of handelaar\ngoede staat\ntrekhaak =2000 kg\nCAR PASS\nwordt gekeurd met roos formulier\n"
+var_price = "5850"
 var_picspath = "car107"
 var_carroserie = ""
 var_month = ""
-var_year = "2010"
-var_gas = "Benzine"
+var_year = "2007"
+var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "83000"
+var_km = "187000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1398"
-var_co2 = "129"
-var_euro = "euro 5"
-var_options = """Centrale vergrendeling
-Elektrische ramen
-Electronic Stability Program (ESP)
-Isofix
-Lichtmetalen velgen
-Metaalkleur
-Navigatiesysteem
-Alarm
+var_cilinder = "1995"
+var_co2 = ""
+var_euro = "Euro 4"
+var_options = """Alarm
+Parkeercamera
 Elektrische buitenspiegels
 Radio
 Traction-control
+Verwarmde buitenspiegels
 ABS
 Airbags
-Boordcomputer"""
+Boordcomputer
+Schuifdeur
+Centrale vergrendeling
+Airconditioning
+Elektrische ramen
+Electronic Stability Program (ESP)
+Metaalkleur
+Navigatiesysteem
+Trekhaak"""
 var_carcolor = ""
 var_interiorcolor = ""

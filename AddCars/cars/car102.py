10f53a7fb441e4e1879a184ac5716198a75ffc18
Filip Luchianov <filip.luchianov@signanthealth.com>
2021-03-24 09:37:00 +0200

    102

--- AddCars/cars/car102.py
+++ AddCars/cars/car102.py
@@ -5,40 +5,48 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "peugeot partner 1.6 benzine,Garantie,CAR PASS,euro 5b"
+var_title = "bmw 318d,Modeljaar 2014,Garantie,CAR PASS,€7434+btw/tva"
 var_categorie = "Auto's"
-var_brand = "Peugeot"
-var_model = "Partner"
-var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "6995"
+var_brand = "BMW"
+var_model = "3 Reeks"
+var_desc = "\nprijs € 7434+btw/tva\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "8995"
 var_picspath = "car102"
-var_carroserie = "Monovolume"
+var_carroserie = ""
 var_month = ""
-var_year = "2014"
-var_gas = "Benzine"
+var_year = "2013"
+var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "118000"
+var_km = "182000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1598"
-var_co2 = "155"
+var_cilinder = "1995"
+var_co2 = "119"
 var_euro = "euro 5"
 var_options = """Alarm
 Elektrische buitenspiegels
+Climate control
 Mistlampen
 Radio
+Start-stop-systeem
 Traction-control
 Verwarmde buitenspiegels
+Xenon verlichting
+Elektrische koffer
 ABS
 Airbags
+Lederen bekleding
 Boordcomputer
-Schuifdeur
 Centrale vergrendeling
 Airconditioning
 Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
+Lichtmetalen velgen
 Metaalkleur
-Parkeersensor"""
+Navigatiesysteem
+Parkeersensor
+Zetelverwarming
+Cruise Control"""
 var_carcolor = ""
 var_interiorcolor = ""

a16c3263b0124a7a572b677ecd0951e578ffa3f9
Filip Luchianov <filip.luchianov@signanthealth.com>
2021-03-25 13:46:19 +0200

    106

--- AddCars/cars/car106.py
+++ AddCars/cars/car106.py
@@ -5,37 +5,43 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "toyota rav4,1.8 benzine,Garantie,CAR PASS,airco,39000 km !!!"
+var_title = "mini cooper CABRIO 1.6 benzine,Garantie,CAR PASS,48000 km"
 var_categorie = "Auto's"
-var_brand = "Toyota"
-var_model = "Rav4"
-var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n\ntoegang LEZ tot 2028\nerste eigenaar met oh-boekje\n\n"
-var_price = "6500"
+var_brand = "Mini"
+var_model = "Cabriolet"
+var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
+var_price = "9990"
 var_picspath = "car106"
 var_carroserie = ""
 var_month = ""
-var_year = "2005"
+var_year = "2009"
 var_gas = "Benzine"
 var_transmissie = "Hand"
-var_km = "39000"
+var_km = "48000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1794"
-var_co2 = "175"
-var_euro = ""
-var_options = """Centrale vergrendeling
+var_cilinder = "1598"
+var_co2 = "137"
+var_euro = "Euro 4"
+var_options = """Alarm
+Bluetooth
+Elektrische buitenspiegels
+Mistlampen
+Radio
+Start-stop-systeem
+Traction-control
+Verwarmde buitenspiegels
+Xenon verlichting
+ABS
+Airbags
+Boordcomputer
+Centrale vergrendeling
 Airconditioning
 Elektrische ramen
 Electronic Stability Program (ESP)
+Isofix
 Lichtmetalen velgen
-Metaalkleur
-Trekhaak
-Alarm
-Elektrische buitenspiegels
-Radio
-Traction-control
-ABS
-Airbags
-Boordcomputer"""
+Open dak
+Parkeersensor"""
 var_carcolor = ""
 var_interiorcolor = ""

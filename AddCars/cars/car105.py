11e9f850e4e24a07fea342fed83a86c4d16510e0
Filip Luchianov <filip.luchianov@signanthealth.com>
2021-04-27 09:27:29 +0300

    105

--- AddCars/cars/car105.py
+++ AddCars/cars/car105.py
@@ -5,31 +5,44 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "citroen c3, 1.1 benzine,Garantie,CAR PASS,55000 km"
+var_title = "peugeot RCZ 1.6 benzine Garantie,CAR PASS,euro 5"
 var_categorie = "Auto's"
-var_brand = "Citroën"
-var_model = "C3"
+var_brand = "Peugeot"
+var_model = "RCZ"
 var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "3490"
+var_price = "11990"
 var_picspath = "car105"
-var_carroserie = ""
+var_carroserie = "Coupé"
 var_month = ""
-var_year = "2008"
+var_year = "2013"
 var_gas = "Benzine"
 var_transmissie = "Hand"
-var_km = "55000"
+var_km = "94000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1124"
-var_co2 = "140"
-var_euro = "euro 4"
-var_options = """ABS
+var_cilinder = "1598"
+var_co2 = ""
+var_euro = "euro 5"
+var_options = """Alarm
+Bluetooth
+Elektrische buitenspiegels
+Mistlampen
+Radio
+Traction-control
+Verwarmde buitenspiegels
+ABS
 Airbags
+Lederen bekleding
 Boordcomputer
+Centrale vergrendeling
+Airconditioning
 Elektrische ramen
 Electronic Stability Program (ESP)
+Isofix
+Lichtmetalen velgen
 Metaalkleur
-Radio
-Traction-control"""
+Navigatiesysteem
+Parkeersensor
+Cruise Control"""
 var_carcolor = ""
 var_interiorcolor = ""

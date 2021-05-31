1eef64e1d65a27d7d806c96c7b33e818a1b79747
Filip Luchianov <filip.luchianov@signanthealth.com>
2021-05-31 19:14:53 +0300

    107

--- AddCars/cars/car107.py
+++ AddCars/cars/car107.py
@@ -5,38 +5,33 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "opel zafira 1.7 cdti Garantie,CAR PASS,panoramische DAK,7zit"
+var_title = "peugeot 5008 ,1.6 hdi,AUTOMAAT,garantie,7 zit,euro5,CAR PASS"
 var_categorie = "Auto's"
-var_brand = "Opel"
-var_model = "Zafira"
+var_brand = "Peugeot"
+var_model = "5008"
 var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "4390"
+var_price = "6850"
 var_picspath = "car107"
 var_carroserie = "Monovolume"
 var_month = ""
-var_year = "2010"
+var_year = "2012"
 var_gas = "Diesel"
-var_transmissie = "Hand"
-var_km = "188000"
+var_transmissie = "Auto"
+var_km = "190000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1686"
-var_co2 = "152"
-var_euro = "euro 4"
-var_options = """Keyless entry
-Alarm
-Bluetooth
+var_cilinder = "1560"
+var_co2 = "125"
+var_euro = "euro 5"
+var_options = """Alarm
 Elektrische buitenspiegels
 Climate control
 Mistlampen
 Radio
-Startonderbreker
-Start-stop-systeem
 Traction-control
 Verwarmde buitenspiegels
 ABS
 Airbags
-Lederen bekleding
 Boordcomputer
 Centrale vergrendeling
 Airconditioning

cf9bb7e0db37737d5a5134867e6b5034e425eeb8
Filip Luchianov <filip.luchianov@signanthealth.com>
2021-06-21 16:52:27 +0300

    sdd

--- AddCars/cars/car108.py
+++ AddCars/cars/car108.py
@@ -5,39 +5,50 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "citroen c3,1.4 hdi,Garantie,CAR PASS"
+var_title = "peugeot 308 GT line,1.2 i,AUTOMAAT,euro 6b"
 var_categorie = "Auto's"
-var_brand = "Citroën"
-var_model = "C3"
-var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "2999"
+var_brand = "Peugeot"
+var_model = "208"
+var_desc = "\n1 jaar garantie\ngekeurd met roos formulier\n"
+var_price = "12500"
 var_picspath = "car108"
 var_carroserie = ""
 var_month = ""
-var_year = "2006"
-var_gas = "Diesel"
-var_transmissie = "Hand"
-var_km = "140000"
+var_year = "2016"
+var_gas = "Benzine"
+var_transmissie = "Auto"
+var_km = "96000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1398"
-var_co2 = "115"
-var_euro = "euro 4"
+var_cilinder = "1199"
+var_co2 = "112"
+var_euro = "euro 6"
 var_options = """Alarm
+Bluetooth
+Parkeercamera
 Elektrische buitenspiegels
 Climate control
+Mistlampen
 Radio
+Sportpakket
 Traction-control
 Verwarmde buitenspiegels
 ABS
 Airbags
+Lederen bekleding
 Boordcomputer
 Centrale vergrendeling
+USB
 Airconditioning
+Panoramadak
 Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
+Lichtmetalen velgen
 Metaalkleur
+Navigatiesysteem
+Parkeersensor
+Sportstoelen
 Cruise Control"""
 var_carcolor = ""
 var_interiorcolor = ""

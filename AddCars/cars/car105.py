0d10088a50daef0e6e840c4ba26f81f0090d987a
Filip Luchianov <filip.luchianov@signanthealth.com>
2021-08-19 23:10:52 +0300

    sdfsf

--- AddCars/cars/car105.py
+++ AddCars/cars/car105.py
@@ -5,40 +5,40 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "peugeot partner 1.6 d,Garantie,CAR PASS"
+var_title = "renault scenic 1.6 benzine,Garantie,CAR PASS"
 var_categorie = "Auto's"
-var_brand = "Peugeot"
-var_model = "Partner"
+var_brand = "Renault"
+var_model = "Scenic"
 var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\neuro 4\n"
-var_price = "4390"
+var_price = "3500"
 var_picspath = "car105"
 var_carroserie = "Monovolume"
 var_month = ""
-var_year = "2010"
-var_gas = "Diesel"
+var_year = "2008"
+var_gas = "Benzine"
 var_transmissie = "Hand"
-var_km = "189000"
+var_km = "137000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1560"
-var_co2 = "149"
+var_cilinder = "1598"
+var_co2 = "182"
 var_euro = "euro 4"
 var_options = """Alarm
 Elektrische buitenspiegels
+Mistlampen
 Radio
+Start-stop-systeem
 Traction-control
 Verwarmde buitenspiegels
 ABS
 Airbags
 Boordcomputer
-Schuifdeur
 Centrale vergrendeling
 Airconditioning
 Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
 Metaalkleur
-Parkeersensor
-Trekhaak"""
+Parkeersensor"""
 var_carcolor = ""
 var_interiorcolor = ""
--- AddCars/cars/car111.py
+++ AddCars/cars/car111.py
@@ -5,29 +5,26 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "peugeot 3008 ALLURE,1.2 i,130 pk,modeljaar 2017,euro 6b"
+var_title = "citroen berlingo 1.2 benzine,Garantie,CAR PASS,5 zit,Euro 6b"
 var_categorie = "Auto's"
-var_brand = "Peugeot"
-var_model = "3008"
-var_desc = "\nbj 12/2016\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "17990"
+var_brand = "Citroën"
+var_model = "Berlingo"
+var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n\neerste
+var_price = "11999"
 var_picspath = "car111"
-var_carroserie = "SUV"
+var_carroserie = "Monovolume"
 var_month = ""
-var_year = "2016"
+var_year = "2017"
 var_gas = "Benzine"
 var_transmissie = "Hand"
-var_km = "46000"
+var_km = "60000"
 var_doors = ""
 var_pk = ""
 var_cilinder = "1199"
-var_co2 = ""
+var_co2 = "119"
 var_euro = "euro 6"
 var_options = """Alarm
-Bluetooth
-Parkeercamera
 Elektrische buitenspiegels
-Climate control
 Mistlampen
 Radio
 Traction-control
@@ -35,19 +32,13 @@ Verwarmde buitenspiegels
 ABS
 Airbags
 Boordcomputer
+Schuifdeur
 Centrale vergrendeling
-USB
 Airconditioning
-Panoramadak
 Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
-Lichtmetalen velgen
-Metaalkleur
-Navigatiesysteem
-Open dak
 Parkeersensor
-Trekhaak
 Cruise Control"""
 var_carcolor = ""
 var_interiorcolor = ""

402da4c5009f9d53b35f770cfa536f6002e86613
DESKTOP-QGBSV83\filip <filip.luchianov@gmail.com>
2021-09-19 21:32:42 +0300

    sdd

--- AddCars/cars/car109.py
+++ AddCars/cars/car109.py
@@ -5,29 +5,27 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "kia venga 1.4 benzine,Garantie,CAR PASS,38000 km,euro 5"
+var_title = "peugeot 208 AUTOMAAT 1.2 benzine,Garantie,CAR PASS,euro 6"
 var_categorie = "Auto's"
-var_brand = "Kia"
-var_model = "Venga"
+var_brand = "peugeot"
+var_model = "208"
 var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "7950"
+var_price = "11500"
 var_picspath = "car109"
 var_carroserie = ""
 var_month = ""
-var_year = "2014"
+var_year = "2017"
 var_gas = "Benzine"
-var_transmissie = "Hand"
-var_km = "38000"
+var_transmissie = "Auto"
+var_km = "67000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1396"
-var_co2 = "140"
-var_euro = "Euro 5"
+var_cilinder = "1199"
+var_co2 = "97"
+var_euro = "Euro 6"
 var_options = """Alarm
 Bluetooth
-Parkeercamera
 Elektrische buitenspiegels
-Mistlampen
 Radio
 Traction-control
 Verwarmde buitenspiegels
@@ -37,14 +35,11 @@ Boordcomputer
 Centrale vergrendeling
 USB
 Airconditioning
-Panoramadak
 Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
 Metaalkleur
 Navigatiesysteem
-Open dak
-Parkeersensor
 Cruise Control"""
 var_carcolor = ""
 var_interiorcolor = ""

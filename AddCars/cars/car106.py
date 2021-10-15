9b5b8f91b3f5e8b940733782a8315b92a4e5e5f9
DESKTOP-QGBSV83\filip <filip.luchianov@gmail.com>
2021-10-15 13:41:02 +0300

    ss

--- AddCars/cars/car106.py
+++ AddCars/cars/car106.py
@@ -5,55 +5,45 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "peugeot 508 RXH 2.0hdi, AUTOMAAT,bj.2018,km 32000,euro 6b"
+var_title = "toyota rav4,CROSS SPORT,d4d,2.2d,bwj. 2008"
 var_categorie = "Auto's"
-var_brand = "Peugeot"
-var_model = "508"
-var_desc = "\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "23900"
+var_brand = "Toyota"
+var_model = "Rav4"
+var_desc = "\nenkel voor export of handelaar\nkeuring geldig tot 05/2022\n"
+var_price = "5350"
 var_picspath = "car106"
-var_carroserie = "Break"
+var_carroserie = "SUV"
 var_month = ""
-var_year = "2018"
+var_year = "2008"
 var_gas = "Diesel"
-var_transmissie = "Auto"
-var_km = "32000"
+var_transmissie = "Hand"
+var_km = "200000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1997"
-var_co2 = "119"
-var_euro = "Euro 6"
-var_options = """Dodehoekdetectie
-Keyless entry
+var_cilinder = "2231"
+var_co2 = "175"
+var_euro = "Euro 4"
+var_options = """4x4
 Alarm
-Bluetooth
-Parkeercamera
 Elektrische buitenspiegels
-Elektrische stoelverstelling
 Climate control
 Mistlampen
 Radio
-Startonderbreker
-Start-stop-systeem
 Traction-control
 Verwarmde buitenspiegels
-Elektrische koffer
 ABS
 Airbags
 Lederen bekleding
 Boordcomputer
 Centrale vergrendeling
-USB
 Airconditioning
-Panoramadak
 Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
 Lichtmetalen velgen
 Metaalkleur
-Navigatiesysteem
 Parkeersensor
-Zetelverwarming
+Trekhaak
 Cruise Control"""
 var_carcolor = ""
 var_interiorcolor = ""

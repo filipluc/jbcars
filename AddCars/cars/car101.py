aaba1b9608daf167dff62eb5bbb25f1a702b3772
DESKTOP-QGBSV83\filip <filip.luchianov@gmail.com>
2021-11-09 16:29:43 +0200

    dd

--- AddCars/cars/car101.py
+++ AddCars/cars/car101.py
@@ -5,27 +5,28 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "opel zafira 1.7 d,Garantie,CAR PASS,7 zetels"
+var_title = "nissan qashqai 1.6 benzine,Garantie,CAR PASS"
 var_categorie = "Auto's"
-var_brand = "Opel"
-var_model = "Zafira"
+var_brand = "Nissan"
+var_model = "Qashqai"
 var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "4950"
+var_price = "6850"
 var_picspath = "car101"
-var_carroserie = "Monovolume"
+var_carroserie = "SUV"
 var_month = ""
-var_year = "2010"
-var_gas = "Diesel"
+var_year = "2009"
+var_gas = "Benzine"
 var_transmissie = "Hand"
-var_km = "133000"
+var_km = "175000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1686"
-var_co2 = "139"
+var_cilinder = "1598"
+var_co2 = "159"
 var_euro = "euro 4"
 var_options = """Alarm
+Parkeercamera
 Elektrische buitenspiegels
-Mistlampen
+Climate control
 Radio
 Traction-control
 Verwarmde buitenspiegels
@@ -37,9 +38,9 @@ Airconditioning
 Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
+Lichtmetalen velgen
 Metaalkleur
-Parkeersensor
-Trekhaak
-Cruise Control"""
+Navigatiesysteem
+Trekhaak"""
 var_carcolor = ""
 var_interiorcolor = ""
--- AddCars/cars/car104.py
+++ AddCars/cars/car104.py
@@ -5,36 +5,44 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "chevrolet aveo 1.2 benzine,Garantie,CAR PASS,72000 km,euro 5"
+var_title = "peugeot 2008,1.2 benzine,Garantie,CAR PASS,euro 5b"
 var_categorie = "Auto's"
-var_brand = "Chevrolet"
-var_model = "Aveo"
-var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n\neerste eigenaar met oh-boekje\n"
-var_price = "5850"
+var_brand = "Peugeot"
+var_model = "2008"
+var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n\nbwj. dec/2014\n60 kw\n\neerste eigenaar met oh-boekje\n"
+var_price = "8499"
 var_picspath = "car104"
-var_carroserie = ""
+var_carroserie = "SUV"
 var_month = ""
-var_year = "2012"
+var_year = "2014"
 var_gas = "Benzine"
 var_transmissie = "Hand"
-var_km = "72000"
+var_km = "108000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1229"
-var_co2 = "129"
+var_cilinder = "1199"
+var_co2 = "114"
 var_euro = "euro 5"
-var_options = """Centrale vergrendeling
+var_options = """Alarm
+Bluetooth
+Elektrische buitenspiegels
+Mistlampen
+Radio
+Traction-control
+Verwarmde buitenspiegels
+ABS
+Airbags
+Boordcomputer
+Centrale vergrendeling
+USB
+Airconditioning
 Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
-Lichtmetalen velgen
 Metaalkleur
-Cruise Control
-Alarm
-Radio
-Traction-control
-ABS
-Airbags
-Boordcomputer"""
+Navigatiesysteem
+Parkeersensor
+Trekhaak
+Cruise Control"""
 var_carcolor = ""
 var_interiorcolor = ""

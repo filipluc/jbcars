f89c5c32a4b20add9a1cdcd48cefb11fa079b704
DESKTOP-QGBSV83\filip <filip.luchianov@gmail.com>
2021-12-13 09:43:02 +0200

    dd

--- AddCars/cars/car104.py
+++ AddCars/cars/car104.py
@@ -5,27 +5,27 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "peugeot 2008,1.2 benzine,Garantie,CAR PASS,euro 5b"
+var_title = "seat altea XL 1.6 d,Garantie,CAR PASS,87000 km,Euro 5"
 var_categorie = "Auto's"
-var_brand = "Peugeot"
-var_model = "2008"
-var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n\nbwj. dec/2014\n60 kw\n\neerste eigenaar met oh-boekje\n"
-var_price = "8499"
+var_brand = "Seat"
+var_model = "Altea"
+var_desc = "\n1 jaar garantie\nCAR PASS\nwordt gekeurd met roos formulier\neerste eigenaar met oh-boekje\n"
+var_price = "6999"
 var_picspath = "car104"
-var_carroserie = "SUV"
+var_carroserie = "Monovolume"
 var_month = ""
-var_year = "2014"
-var_gas = "Benzine"
+var_year = "2010"
+var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "108000"
+var_km = "87000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1199"
-var_co2 = "114"
+var_cilinder = "1598"
+var_co2 = "119"
 var_euro = "euro 5"
 var_options = """Alarm
-Bluetooth
 Elektrische buitenspiegels
+Climate control
 Mistlampen
 Radio
 Traction-control
@@ -34,11 +34,11 @@ ABS
 Airbags
 Boordcomputer
 Centrale vergrendeling
-USB
 Airconditioning
 Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
+Lichtmetalen velgen
 Metaalkleur
 Navigatiesysteem
 Parkeersensor

07b7f3f91aa001defb619e1104d6b3a78262374c
DESKTOP-QGBSV83\filip <filip.luchianov@gmail.com>
2022-01-13 10:22:24 +0200

    dfdf

--- AddCars/cars/car109.py
+++ AddCars/cars/car109.py
@@ -5,37 +5,49 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "peugeot 207+,1.4 benzine,Garantie,CAR PASS,euro 5"
+var_title = "renault kadjar 1.2 benzine,Garantie,CAR PASS,euro 6b"
 var_categorie = "Auto's"
-var_brand = "Peugeot"
-var_model = "207"
+var_brand = "Renault"
+var_model = "Kadjar"
 var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "4999"
+var_price = "14999"
 var_picspath = "car109"
-var_carroserie = ""
+var_carroserie = "SUV"
 var_month = ""
-var_year = "2013"
+var_year = "2016"
 var_gas = "Benzine"
 var_transmissie = "Hand"
-var_km = "112000"
+var_km = "57000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1360"
-var_co2 = "145"
-var_euro = "Euro 5"
-var_options = """Alarm
+var_cilinder = "1197"
+var_co2 = "130"
+var_euro = "Euro 6"
+var_options = """Keyless entry
+Alarm
+Bluetooth
 Elektrische buitenspiegels
+Climate control
 Mistlampen
 Radio
+Start-stop-systeem
+Traction-control
 Verwarmde buitenspiegels
 ABS
 Airbags
+Lederen bekleding
 Boordcomputer
 Centrale vergrendeling
+USB
 Airconditioning
 Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
+Lichtmetalen velgen
+Metaalkleur
+Navigatiesysteem
+Parkeersensor
+Trekhaak
 Cruise Control"""
 var_carcolor = ""
 var_interiorcolor = ""

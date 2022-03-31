5ae693b90cf201a099ea5724ee0497f91168fb54
DESKTOP-QGBSV83\filip <filip.luchianov@gmail.com>
2022-03-31 18:34:43 +0300

    dfg

--- AddCars/cars/car106.py
+++ AddCars/cars/car106.py
@@ -5,52 +5,39 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "skoda superb 1.6d,Garantie,CAR PASS,euro 5b"
+var_title = "skoda roomster 1.2 benzine,Garantie,CAR PASS,airco"
 var_categorie = "Auto's"
 var_brand = "Skoda"
-var_model = "Superb"
-var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "12990"
+var_model = "Roomster"
+var_desc = "\n1 jaar garantie\nCAR PASS\nwordt gekeurd met roos formulier\neerste eigenaar met oh-boekje\n"
+var_price = "4850"
 var_picspath = "car106"
-var_carroserie = "Break"
+var_carroserie = ""
 var_month = ""
-var_year = "2015"
-var_gas = "Diesel"
+var_year = "2009"
+var_gas = "Benzine"
 var_transmissie = "Hand"
-var_km = "120000"
+var_km = "100000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1598"
-var_co2 = "113"
-var_euro = "Euro 5"
+var_cilinder = "1198"
+var_co2 = "155"
+var_euro = "Euro 4"
 var_options = """Alarm
-Bluetooth
 Elektrische buitenspiegels
-Elektrische stoelverstelling
-Climate control
 Mistlampen
 Radio
 Traction-control
 Verwarmde buitenspiegels
-Xenon verlichting
 ABS
 Airbags
-Lederen bekleding
 Boordcomputer
 Centrale vergrendeling
-USB
 Airconditioning
-Panoramadak
 Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
-Lichtmetalen velgen
 Metaalkleur
-Navigatiesysteem
-Open dak
-Parkeersensor
-Zetelverwarming
-Trekhaak
 Cruise Control"""
 var_carcolor = ""
 var_interiorcolor = ""

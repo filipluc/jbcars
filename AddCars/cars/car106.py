72b9c88dac107b1894eed6f3a766e009f494df73
DESKTOP-QGBSV83\filip <filip.luchianov@gmail.com>
2022-05-09 16:17:49 +0300

    tst2

--- AddCars/cars/car106.py
+++ AddCars/cars/car106.py
@@ -5,30 +5,29 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "skoda roomster 1.2 benzine,Garantie,CAR PASS,airco"
+var_title = "renault clio 1.4 benzine,AIRCO,export !!!"
 var_categorie = "Auto's"
-var_brand = "Skoda"
-var_model = "Roomster"
-var_desc = "\n1 jaar garantie\nCAR PASS\nwordt gekeurd met roos formulier\neerste eigenaar met oh-boekje\n"
-var_price = "4850"
+var_brand = "renault"
+var_model = "Clio"
+var_desc = "\nexport of handelaar\neerste eigenaar 02/12/2004\n72 kw\n"
+var_price = "1250"
 var_picspath = "car106"
 var_carroserie = ""
 var_month = ""
-var_year = "2009"
+var_year = "2004"
 var_gas = "Benzine"
 var_transmissie = "Hand"
-var_km = "100000"
+var_km = "200000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1198"
-var_co2 = "155"
+var_cilinder = "1390"
+var_co2 = "160"
 var_euro = "Euro 4"
 var_options = """Alarm
 Elektrische buitenspiegels
 Mistlampen
 Radio
 Traction-control
-Verwarmde buitenspiegels
 ABS
 Airbags
 Boordcomputer
@@ -37,7 +36,7 @@ Airconditioning
 Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
-Metaalkleur
-Cruise Control"""
+Lichtmetalen velgen
+Metaalkleur"""
 var_carcolor = ""
 var_interiorcolor = ""

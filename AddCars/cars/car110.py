df7df799810e8ff032dcedd4bd4ffdb8444d95b4
DESKTOP-QGBSV83\filip <filip.luchianov@gmail.com>
2022-05-30 20:11:48 +0300

    fdgfd

--- AddCars/cars/car110.py
+++ AddCars/cars/car110.py
@@ -5,37 +5,42 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "ford focus 1.0 i,Bwj 2013,START NIET,Demarre pas,Export"
+var_title = "mazda CX-5,2.2d,bwj.2013,Euro 6"
 var_categorie = "Auto's"
-var_brand = "Ford"
-var_model = "Focus"
-var_desc = "\nStart niet/demarre pas !!!!\ngebroken distributieriem !!!!\ncourroie de distribution cassee !!!\n74 kw\n"
-var_price = "3850"
+var_brand = "Mazda"
+var_model = "CX-5"
+var_desc = "\nenkel voor handelaar of export\nkeuring geldig tot 02/01/2023\neerste eigenaar\n"
+var_price = "9999"
 var_picspath = "car110"
-var_carroserie = "Break"
+var_carroserie = "SUV"
 var_month = ""
 var_year = "2013"
-var_gas = "Benzine"
+var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "117000"
+var_km = "220000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "998"
-var_co2 = "112"
-var_euro = "euro 5"
+var_cilinder = "2191"
+var_co2 = "119"
+var_euro = "euro 6"
 var_options = """Alarm
 Elektrische buitenspiegels
+Mistlampen
 Radio
 Traction-control
+Verwarmde buitenspiegels
 ABS
 Airbags
+Lederen bekleding
 Boordcomputer
 Centrale vergrendeling
-Airconditioning
 Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
+Lichtmetalen velgen
 Metaalkleur
-Parkeersensor"""
+Parkeersensor
+Zetelverwarming
+Cruise Control"""
 var_carcolor = ""
 var_interiorcolor = ""

7d2d65e35be8a32d830d51b563519c7bd14da93d
DESKTOP-QGBSV83\filip <filip.luchianov@gmail.com>
2022-06-06 12:42:56 +0300

    fgf

--- AddCars/cars/car112.py
+++ AddCars/cars/car112.py
@@ -5,46 +5,31 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "peugeot 308,1.6 hdi,7 zit,Garantie,CAR PASS,navi,pano,Euro 5"
+var_title = "citroen jumper 2.2 hdi,150 pk,7 zit,bwj.2014,euro 5"
 var_categorie = "Auto's"
-var_brand = "Peugeot"
-var_model = "308"
-var_desc = "\n7 zetels\n1 jaar garantie\nCAR PASS\nwordt gekeurd met roos formulier\neerste eigenaar met oh-boekje bij Peugeot\nIn jaar 2021 groot onderhoud:\n-distributieriem+waterpomp\n-koppeling+vliegwiel\n"
-var_price = "7650"
+var_brand = "Citroën"
+var_model = "Jumper"
+var_desc = "\enkel voor export of handelaar\nCAR PASS\nkeuring voor verkoop +500 euro\n"
+var_price = "8800"
 var_picspath = "car112"
-var_carroserie = "Break"
+var_carroserie = ""
 var_month = ""
 var_year = "2014"
 var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "121000"
+var_km = "237000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1560"
-var_co2 = "119"
+var_cilinder = "2198"
+var_co2 = ""
 var_euro = "euro 5"
-var_options = """Alarm
-Bluetooth
-Elektrische buitenspiegels
-Climate control
-Mistlampen
+var_options = """Centrale vergrendeling
+Elektrische ramen
+Trekhaak
+Alarm
 Radio
-Traction-control
-Verwarmde buitenspiegels
 ABS
 Airbags
-Boordcomputer
-Centrale vergrendeling
-Airconditioning
-Panoramadak
-Elektrische ramen
-Electronic Stability Program (ESP)
-Isofix
-Lichtmetalen velgen
-Metaalkleur
-Navigatiesysteem
-Parkeersensor
-Trekhaak
-Cruise Control"""
+Boordcomputer"""
 var_carcolor = ""
 var_interiorcolor = ""
--- AddCars/cars/car113.py
+++ AddCars/cars/car113.py
@@ -5,47 +5,31 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "hyundai i30,1.6 crdi,Garantie,CAR PASS,pano,navi,camera"
+var_title = "citroen jumper 2.2 hdi,Euro 5,bwj.2014///7 zetels"
 var_categorie = "Auto's"
-var_brand = "Hyundai"
-var_model = "i30"
-var_desc = "\n1 jaar garantie\nCAR PASS\nwordt gekeurd met roos formulier\neerste eigenaar met oh-boekje\n81 kw\n"
-var_price = "8990"
+var_brand = "Bestelwagens en Lichte vracht"
+var_model = "Citroën"
+var_desc = "\nexport of handelaar\nkeuring voor verkoop +500 euro\n110 kw/150pk\n"
+var_price = "8800"
 var_picspath = "car113"
 var_carroserie = ""
 var_month = ""
 var_year = "2014"
 var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "99000"
+var_km = "237000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1582"
-var_co2 = "108"
-var_euro = "euro 5"
-var_options = """Alarm
-Parkeercamera
-Elektrische buitenspiegels
-Climate control
-Mistlampen
+var_cilinder = "2128"
+var_co2 = ""
+var_euro = "Euro 5"
+var_options = """Centrale vergrendeling
+Elektrische ramen
+Trekhaak
+Alarm
 Radio
-Traction-control
-Verwarmde buitenspiegels
 ABS
 Airbags
-Boordcomputer
-Centrale vergrendeling
-USB
-Airconditioning
-Panoramadak
-Elektrische ramen
-Electronic Stability Program (ESP)
-Isofix
-Lichtmetalen velgen
-Metaalkleur
-Navigatiesysteem
-Open dak
-Parkeersensor
-Cruise Control"""
+Boordcomputer"""
 var_carcolor = ""
 var_interiorcolor = ""

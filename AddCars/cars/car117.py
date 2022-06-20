5b13862ee8a19f037c0f6751b3ad405b92d42b8e
DESKTOP-QGBSV83\filip <filip.luchianov@gmail.com>
2022-06-20 19:56:28 +0300

    err

--- AddCars/cars/car117.py
+++ AddCars/cars/car117.py
@@ -5,23 +5,23 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "opel combo 1.4 benzine ,Garantie,CAR PASS,airco"
+var_title = "opel combo 1.3 cdti,Garantie,CAR PASS,airco"
 var_categorie = "Auto's"
 var_brand = "Opel"
 var_model = "Combo Tour"
-var_desc = "\n1 jaar garantie\nCAR PASS\nwordt gekeurd met roos formulier\neerste eigenaar met oh-boekje\n"
-var_price = "4999"
+var_desc = "\n1 jaar garantie\nCAR PASS\nwordt gekeurd met roos formulier\n"
+var_price = "4500"
 var_picspath = "car117"
 var_carroserie = "Monovolume"
 var_month = ""
-var_year = "2007"
-var_gas = "Benzine"
+var_year = "2008"
+var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "112000"
+var_km = "141000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1364"
-var_co2 = "151"
+var_cilinder = "1248"
+var_co2 = "138"
 var_euro = "euro 4"
 var_options = """Alarm
 Radio
@@ -29,11 +29,11 @@ Traction-control
 ABS
 Airbags
 Boordcomputer
-Schuifdeur
 Centrale vergrendeling
 Airconditioning
 Elektrische ramen
 Electronic Stability Program (ESP)
-Isofix"""
+Isofix
+Metaalkleur"""
 var_carcolor = ""
 var_interiorcolor = ""

5f66e777a28af5f5f39423e5e602ad47a851b79c
DESKTOP-QGBSV83\filip <filip.luchianov@gmail.com>
2022-07-13 15:08:47 +0300

    dfg

--- AddCars/cars/car122.py
+++ AddCars/cars/car122.py
@@ -5,50 +5,37 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "ford kuga 2.0 d,Garantie,CAR PASS,bwj.2014,euro 5b"
+var_title = "kia cee’d 1.6 crdi break EURO 5,/bwj 2013"
 var_categorie = "Auto's"
-var_brand = "Ford"
-var_model = "Kuga"
-var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\neerste eigenaar met oh-boekje\n"
-var_price = "10650"
+var_brand = "Kia"
+var_model = "(Pro) Cee d"
+var_desc = "\nzo meenemen\n"
+var_price = "5950"
 var_picspath = "car122"
-var_carroserie = "SUV"
+var_carroserie = "Break"
 var_month = ""
-var_year = "2014"
+var_year = "2013"
 var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "131000"
+var_km = "177000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1997"
-var_co2 = "139"
+var_cilinder = "1582"
+var_co2 = "113"
 var_euro = "euro 5"
-var_options = """Keyless entry
-Alarm
-Bluetooth
+var_options = """Alarm
 Elektrische buitenspiegels
-Elektrische stoelverstelling
-Climate control
 Mistlampen
 Radio
-Start-stop-systeem
 Traction-control
-Verwarmde buitenspiegels
-Elektrische koffer
 ABS
 Airbags
-Lederen bekleding
 Boordcomputer
 Centrale vergrendeling
 Airconditioning
-Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
-Lichtmetalen velgen
 Metaalkleur
-Navigatiesysteem
-Parkeersensor
-Zetelverwarming
 Cruise Control"""
 var_carcolor = ""
 var_interiorcolor = ""

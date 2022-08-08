11e621603381cb301407c525e3cfabf7b682d3be
DESKTOP-QGBSV83\filip <filip.luchianov@gmail.com>
2022-08-08 13:34:54 +0300

    sdg

--- AddCars/cars/car108.py
+++ AddCars/cars/car108.py
@@ -5,36 +5,50 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "citroen c1, 1.0 benzine,Garantie,CAR PASS,airco,bwj 2014"
+var_title = "audi A5 cabrio,2.0 benzine,S line,211 pk,Euro 5"
 var_categorie = "Auto's"
-var_brand = "Citroën"
-var_model = "C1"
-var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\neerste eigenaar met oh-boekje\n"
-var_price = "5999"
+var_brand = "Audi"
+var_model = "A5"
+var_desc = "\nCAR PASS\nwordt gekeurd met roos formulier\n"
+var_price = "15000"
 var_picspath = "car108"
-var_carroserie = ""
+var_carroserie = "Cabriolet"
 var_month = ""
-var_year = "2014"
+var_year = "2009"
 var_gas = "Benzine"
 var_transmissie = "Hand"
-var_km = "42000"
+var_km = "117000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "998"
-var_co2 = "99"
+var_cilinder = "1984"
+var_co2 = "159"
 var_euro = "euro 5"
 var_options = """Alarm
+Bluetooth
+Elektrische buitenspiegels
+Elektrische stoelverstelling
+Climate control
+Mistlampen
 Radio
+Sportpakket
 Traction-control
+Verwarmde buitenspiegels
 ABS
 Airbags
+Lederen bekleding
 Boordcomputer
 Centrale vergrendeling
+USB
 Airconditioning
 Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
 Lichtmetalen velgen
-Metaalkleur"""
+Metaalkleur
+Navigatiesysteem
+Parkeersensor
+Sportstoelen
+Zetelverwarming
+Cruise Control"""
 var_carcolor = ""
 var_interiorcolor = ""

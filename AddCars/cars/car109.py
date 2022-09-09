d30aab09401f1f99b810040f7eba2a8e12eafa3f
DESKTOP-QGBSV83\filip <filip.luchianov@gmail.com>
2022-09-09 16:42:18 +0300

    dfg

--- AddCars/cars/car109.py
+++ AddCars/cars/car109.py
@@ -5,51 +5,35 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "peugeot 4008 ,1.6 hdi,4x4,Garantie,CAR PASS,57000 km"
+var_title = "skoda fabia 1.2 diesel,Euro 5,airco,EXPORT"
 var_categorie = "Auto's"
-var_brand = "Peugeot"
-var_model = "4008"
-var_desc = "\n1 jaar garantie\nCAR PASS\nwordt gekeurd met roos formulier\n"
+var_brand = "Skoda"
+var_model = "Fabia"
+var_desc = "\nEXPORT\n"
 var_price = "15830"
 var_picspath = "car109"
-var_carroserie = "SUV"
+var_carroserie = ""
 var_month = ""
-var_year = "2014"
+var_year = "2011"
 var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "57000"
+var_km = "176000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1560"
-var_co2 = "129"
+var_cilinder = "1199"
+var_co2 = "99"
 var_euro = "Euro 5"
-var_options = """Keyless entry
-4x4
-Alarm
-Bluetooth
-Parkeercamera
-Elektrische buitenspiegels
-Mistlampen
-Radio
-Start-stop-systeem
-Traction-control
-Verwarmde buitenspiegels
-Xenon verlichting
-ABS
-Airbags
-Lederen bekleding
-Boordcomputer
-Centrale vergrendeling
+var_options = """Centrale vergrendeling
 Airconditioning
-Panoramadak
 Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
-Lichtmetalen velgen
-Metaalkleur
-Navigatiesysteem
-Parkeersensor
-Zetelverwarming
-Cruise Control"""
+Alarm
+Elektrische buitenspiegels
+Radio
+Traction-control
+ABS
+Airbags
+Boordcomputer"""
 var_carcolor = ""
 var_interiorcolor = ""

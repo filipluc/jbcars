3336e3d7b2e577c4e75e5f16d783d179fb32ba4d
DESKTOP-QGBSV83\filip <filip.luchianov@gmail.com>
2022-09-21 17:27:03 +0300

    sdg

--- AddCars/cars/car115.py
+++ AddCars/cars/car115.py
@@ -5,25 +5,34 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "citroen c2, 1.1 benzine,CAR PASS,49000 km"
+var_title = "peugeot partner 1.6 hdi AUTOMAAT,export of autohandel,euro 5"
 var_categorie = "Auto's"
-var_brand = "Citroën"
-var_model = "C2"
-var_desc = "\n1 jaar garantie\nCAR PASS\nwordt gekeurd met roos formulier\neerste eigenaar met oh-boekje\ndistributie riem vervangen op 39000 km(jaar 2019)\n"
-var_price = "3290"
+var_brand = "Peugeot"
+var_model = "Partner"
+var_desc = "\nenkel voor export of autohandel\nFAP roetfilter storing/lawaai motor\neuro 5\neerste eigenaar met oh-boekje\n68 kw\nautomaat\n"
+var_price = "3500"
 var_picspath = "car115"
 var_carroserie = ""
 var_month = ""
-var_year = "2006"
-var_gas = "Benzine"
-var_transmissie = "Hand"
-var_km = "49000"
+var_year = "2012"
+var_gas = "Diesel"
+var_transmissie = "Auto"
+var_km = "217000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1124"
-var_co2 = "138"
-var_euro = "euro 4"
-var_options = """Metaalkleur
+var_cilinder = "1560"
+var_co2 = ""
+var_euro = "euro 5"
+var_options = """Centrale vergrendeling
+Airconditioning
+Elektrische ramen
+Electronic Stability Program (ESP)
+Isofix
+Lichtmetalen velgen
+Metaalkleur
+Cruise Control
+Alarm
+Elektrische buitenspiegels
 Radio
 ABS
 Airbags

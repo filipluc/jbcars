922cd9574c776e55fe1489130eacf639b0df3c18
DESKTOP-QGBSV83\filip <filip.luchianov@gmail.com>
2022-10-17 19:22:43 +0300

    sdgfs

--- AddCars/addcarTOATE.py
+++ AddCars/addcarTOATE.py
@@ -136,8 +136,8 @@ class AdaugaMasini(unittest.TestCase):
     def test_car116(self):
         GeneralFunctions.deleteAddCarFunction(self, car116)
 
-    def test_car117(self):
-        GeneralFunctions.deleteAddCarFunction(self, car117)
+    # def test_car117(self):
+    #     GeneralFunctions.deleteAddCarFunction(self, car117)
 
     def test_car118(self):
         GeneralFunctions.deleteAddCarFunction(self, car118)

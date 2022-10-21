267aa95ebc5fe55c304185bce0959174399d1066
DESKTOP-QGBSV83\filip <filip.luchianov@gmail.com>
2022-10-21 10:47:02 +0300

    sdf

--- AddCars/cars/car117.py
+++ AddCars/cars/car117.py
@@ -5,34 +5,40 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "peugeot 207 sw,1.6 hdi,CAR PASS,61000 km,Garantie"
+var_title = "opel corsa 1.4 i,90 pk,Euro 6b,Garantie,CAR PASS,airco,NAVI"
 var_categorie = "Auto's"
-var_brand = "Peugeot"
-var_model = "207"
-var_desc = "\n1 jaar garantie\nCAR PASS\nwordt gekeurd met roos formulier\neerste eigenaar met oh-boekje\n"
-var_price = "4995"
+var_brand = "Opel"
+var_model = "Corsa"
+var_desc = "\n1 jaar garantie\nCAR PASS\nwordt gekeurd met roos formulier\neerste eigenaar met oh-boekje bij OPEL\ndistributie op ketting\n66 kw\n"
+var_price = "9999"
 var_picspath = "car117"
 var_carroserie = ""
 var_month = ""
-var_year = "2009"
-var_gas = "Diesel"
+var_year = "2017"
+var_gas = "Benzine"
 var_transmissie = "Hand"
-var_km = "61000"
+var_km = "65000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1560"
-var_co2 = "119"
-var_euro = "euro 4"
-var_options = """Centrale vergrendeling
+var_cilinder = "1398"
+var_co2 = "120"
+var_euro = "euro 6"
+var_options = """Alarm
+Bluetooth
+Elektrische buitenspiegels
+Radio
+Traction-control
+Verwarmde buitenspiegels
+ABS
+Airbags
+Boordcomputer
+Centrale vergrendeling
+USB
+Airconditioning
 Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
 Metaalkleur
-Alarm
-Radio
-Traction-control
-ABS
-Airbags
-Boordcomputer"""
+Navigatiesysteem"""
 var_carcolor = ""
 var_interiorcolor = ""

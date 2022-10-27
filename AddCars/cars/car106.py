8e008618bc5b0cdddffaf5959bd4d13eba0bd4ec
DESKTOP-QGBSV83\filip <filip.luchianov@gmail.com>
2022-10-27 21:20:36 +0300

    cvb

--- AddCars/cars/car106.py
+++ AddCars/cars/car106.py
@@ -5,31 +5,34 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "renault clio 1.4 benzine,AIRCO,export !!!"
+var_title = "audi a4,break,2.0 tdi-136 pk,Leder,Airco,EXPORT of Handelaar"
 var_categorie = "Auto's"
-var_brand = "Renault"
-var_model = "Clio"
-var_desc = "\nexport of handelaar\neerste eigenaar 02/12/2004\n72 kw\n"
-var_price = "1250"
+var_brand = "Audi"
+var_model = "A4"
+var_desc = "\nexport of handelaar\nnog in goede staat\n"
+var_price = "2950"
 var_picspath = "car106"
-var_carroserie = ""
+var_carroserie = "Break"
 var_month = ""
-var_year = "2004"
-var_gas = "Benzine"
+var_year = "2006"
+var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "200000"
+var_km = "341000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1390"
-var_co2 = "160"
+var_cilinder = "1968"
+var_co2 = "164"
 var_euro = "Euro 4"
 var_options = """Alarm
 Elektrische buitenspiegels
+Climate control
 Mistlampen
 Radio
 Traction-control
+Verwarmde buitenspiegels
 ABS
 Airbags
+Lederen bekleding
 Boordcomputer
 Centrale vergrendeling
 Airconditioning
@@ -37,6 +40,10 @@ Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
 Lichtmetalen velgen
-Metaalkleur"""
+Metaalkleur
+Parkeersensor
+Zetelverwarming
+Trekhaak
+Cruise Control"""
 var_carcolor = ""
 var_interiorcolor = ""
--- AddCars/cars/car107.py
+++ AddCars/cars/car107.py
@@ -5,46 +5,35 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "renault captur 1.2 i,AUTOMAAT,euro 6b,GEKEURD,12.000 km !!!!"
+var_title = "Renault modus 1.1 i,CAR PASS,garantie ,airco,95000 km"
 var_categorie = "Auto's"
 var_brand = "Renault"
-var_model = "Captur"
-var_desc = "\nAUTOMAAT !!!\nnieuwe staat\nCAR PASS\ngekeurd met roos formulier\n"
-var_price = "15999"
+var_model = "Modus"
+var_desc = "\n1 jaar garantie\nCAR PASS\nwordt gekeurd met roos formulier\n"
+var_price = "4950"
 var_picspath = "car107"
-var_carroserie = "SUV"
+var_carroserie = ""
 var_month = ""
-var_year = "2016"
+var_year = "2010"
 var_gas = "Benzine"
-var_transmissie = "Auto"
-var_km = "12700"
+var_transmissie = "Hand"
+var_km = "95000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1197"
+var_cilinder = "1149"
 var_co2 = ""
-var_euro = "euro 6"
-var_options = """Keyless entry
-Alarm
-Bluetooth
-Elektrische buitenspiegels
-Mistlampen
-Radio
-Start-stop-systeem
-Traction-control
-Verwarmde buitenspiegels
-ABS
-Airbags
-Boordcomputer
-Centrale vergrendeling
-USB
+var_euro = "euro 4"
+var_options = """Centrale vergrendeling
 Airconditioning
 Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
-Lichtmetalen velgen
 Metaalkleur
-Navigatiesysteem
-Parkeersensor
-Cruise Control"""
+Alarm
+Elektrische buitenspiegels
+Radio
+Traction-control
+Airbags
+Boordcomputer"""
 var_carcolor = ""
 var_interiorcolor = ""

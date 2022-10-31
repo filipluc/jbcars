468f19c374465cca31f2e5abbb020421b87d99f3
DESKTOP-QGBSV83\filip <filip.luchianov@gmail.com>
2022-10-31 13:53:48 +0200

    dff

--- AddCars/cars/car113.py
+++ AddCars/cars/car113.py
@@ -5,30 +5,28 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "citroen c3,1.4 benzine,AUTOMAAT,garantie,GEKEURD"
+var_title = "nissan almera 1.5 benzine,Garantie,CAR PASS,airco"
 var_categorie = "Auto's"
-var_brand = "Citroën"
-var_model = "C3"
-var_desc = "\nLEZ ok tot 31/12/2027\n1 jaar garantie\nCAR PASS\nwordt gekeurd met roos formulier\n"
-var_price = "4999"
+var_brand = "Nissan"
+var_model = "Almera"
+var_desc = "\n1 jaar garantie\nCAR PASS\nwordt gekeurd met roos formulier\n"
+var_price = "3500"
 var_picspath = "car113"
 var_carroserie = ""
 var_month = ""
-var_year = "2004"
+var_year = "2002"
 var_gas = "Benzine"
-var_transmissie = "Auto"
-var_km = "76000"
+var_transmissie = "Hand"
+var_km = "154000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1360"
-var_co2 = "161"
-var_euro = ""
+var_cilinder = "1498"
+var_co2 = ""
+var_euro = "euro 4"
 var_options = """Alarm
 Elektrische buitenspiegels
-Climate control
 Radio
 Traction-control
-Verwarmde buitenspiegels
 ABS
 Airbags
 Boordcomputer
@@ -36,9 +34,6 @@ Centrale vergrendeling
 Airconditioning
 Elektrische ramen
 Electronic Stability Program (ESP)
-Isofix
-Metaalkleur
-Trekhaak
-Cruise Control"""
+Metaalkleur"""
 var_carcolor = ""
 var_interiorcolor = ""

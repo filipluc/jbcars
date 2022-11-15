d3cd40021cf3a99aaf1c5d252c1f41f60fc5fad8
DESKTOP-QGBSV83\filip <filip.luchianov@gmail.com>
2022-11-15 15:02:12 +0200

    sdgf

--- AddCars/cars/car116.py
+++ AddCars/cars/car116.py
@@ -5,38 +5,45 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "peugeot partner 1.6 i,GEKEURD,garantie,Navigatie,Airco,euro5"
+var_title = "vw beetle CABRIO 1.6 d,50s edition,71000 km,GARANTIE"
 var_categorie = "Auto's"
-var_brand = "Peugeot"
-var_model = "Partner"
-var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\neerste eigenaar met oh-boekje bij dealer\njuist groot onderhoud gehaad\n"
-var_price = "7490"
+var_brand = "Volkswagen"
+var_model = "Beetle (Kever)"
+var_desc = "\n1 jaar garantie\nCAR PASS\nwordt gekeurd met roos formulier\neerste eigenaar met oh-boekje\n"
+var_price = "14950"
 var_picspath = "car116"
-var_carroserie = "Monovolume"
+var_carroserie = "Cabriolet"
 var_month = ""
-var_year = "2014"
-var_gas = "Benzine"
+var_year = "2013"
+var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "158000"
+var_km = "71000"
 var_doors = ""
 var_pk = ""
 var_cilinder = "1598"
-var_co2 = "155"
+var_co2 = "124"
 var_euro = "euro 5"
 var_options = """Alarm
 Bluetooth
+Elektrische buitenspiegels
+Climate control
+Mistlampen
 Radio
 Traction-control
+Verwarmde buitenspiegels
 ABS
 Airbags
+Lederen bekleding
 Boordcomputer
 Centrale vergrendeling
-USB
 Airconditioning
 Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
+Metaalkleur
 Navigatiesysteem
+Parkeersensor
+Zetelverwarming
 Cruise Control"""
 var_carcolor = ""
 var_interiorcolor = ""

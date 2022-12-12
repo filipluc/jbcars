b1d603759a4d5465a280829630758673663fb6d5
DESKTOP-QGBSV83\filip <filip.luchianov@gmail.com>
2022-12-12 10:54:05 +0200

    rgr

--- AddCars/cars/car116.py
+++ AddCars/cars/car116.py
@@ -5,38 +5,42 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "peugeot 3008, 1.6 hdi,GEKEURD,euro 5,Garantie"
+var_title = "nissan micra 1.2 i,Garantie,CAR PASS,euro 5,NAVIGATIE,5 d"
 var_categorie = "Auto's"
-var_brand = "Peugeot"
-var_model = "3008"
-var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\nDistributie riem + waterpomp vervangen (142000 km)\n"
-var_price = "6499"
+var_brand = "Nissan"
+var_model = "Micra"
+var_desc = "\n1 jaar garantie\nCAR PASS\nwordt gekeurd met roos formulier\n
+var_price = "4950"
 var_picspath = "car116"
 var_carroserie = ""
 var_month = ""
 var_year = "2011"
-var_gas = "Diesel"
+var_gas = "Benzine"
 var_transmissie = "Hand"
-var_km = "181000"
+var_km = "178000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1560"
-var_co2 = "135"
+var_cilinder = "1198"
+var_co2 = "99"
 var_euro = "euro 5"
 var_options = """Alarm
+Bluetooth
 Elektrische buitenspiegels
+Climate control
 Radio
 Traction-control
+Verwarmde buitenspiegels
 ABS
 Airbags
 Boordcomputer
 Centrale vergrendeling
+USB
 Airconditioning
 Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
 Metaalkleur
-Trekhaak
+Navigatiesysteem
 Cruise Control"""
 var_carcolor = ""
 var_interiorcolor = ""

859dc6f9ed9ddaad44125354913c7e228e6418c1
Filip Luchianov <filip.luchianov@gmail.com>
2023-01-05 10:58:44 +0200

    dfg

--- AddCars/cars/car109.py
+++ AddCars/cars/car109.py
@@ -5,25 +5,26 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "peugeot 3008,1.6hdi,CAR PASS,garantie,Euro 5,NAVIGATIE,PANO"
+var_title = "peugeot partner 1.6 i,GEKEURD,garantie,CAR PASS,bwj 12/2014"
 var_categorie = "Auto's"
 var_brand = "Peugeot"
-var_model = "3008"
-var_desc = "\nprijs 5781 € +btw/tva\n1 jaar garantie\nCAR PASS\nwordt gekeurd met roos formulier\n114000 km\n82 kw\n"
-var_price = "6995"
+var_model = "Partner"
+var_desc = "\n1 jaar garantie\nCAR PASS\gekeurd met roos formulier\n72 kw\n"
+var_price = "8995"
 var_picspath = "car109"
-var_carroserie = "SUV"
+var_carroserie = "Monovolume"
 var_month = ""
-var_year = "2011"
-var_gas = "Diesel"
+var_year = "2014"
+var_gas = "Benzine"
 var_transmissie = "Hand"
-var_km = "114000"
+var_km = "147000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1560"
-var_co2 = "135"
+var_cilinder = "1598"
+var_co2 = "155"
 var_euro = "Euro 5"
 var_options = """Alarm
+Bluetooth
 Elektrische buitenspiegels
 Climate control
 Mistlampen
@@ -34,15 +35,14 @@ ABS
 Airbags
 Boordcomputer
 Centrale vergrendeling
+USB
 Airconditioning
-Panoramadak
 Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
-Lichtmetalen velgen
 Metaalkleur
 Navigatiesysteem
 Parkeersensor
-Cruise Control"""
+Trekhaak"""
 var_carcolor = ""
 var_interiorcolor = ""

b04d5d2dc5f88ddb11a06c3fccdeffac32c60a9a
Filip Luchianov <filip.luchianov@gmail.com>
2023-01-09 09:07:55 +0200

    sgd

--- AddCars/cars/car102.py
+++ AddCars/cars/car102.py
@@ -5,44 +5,39 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "citroen berlingo XTR 1.6 hdi,Garantie,CAR PASS,71000 km"
+var_title = "citroen c4 picasso 1.6 hdi,7 zit,CAR PASS,garantie"
 var_categorie = "Auto's"
 var_brand = "Citroën"
-var_model = "Berlingo"
-var_desc = "\n1 jaar garantie\nCAR PASS\nwordt gekeurd met roos formulier\n68 kw\neerste eigenaar met oh-boekje\n"
-var_price = "12995"
+var_model = "C4"
+var_desc = "\n1 jaar garantie\nCAR PASS\nwordt gekeurd met roos formulier\n80 kw\n7 zit\n144000 km\n"
+var_price = "5995"
 var_picspath = "car102"
 var_carroserie = "Monovolume"
 var_month = ""
-var_year = "2015"
+var_year = "2009"
 var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "71000"
+var_km = "144000"
 var_doors = ""
 var_pk = ""
 var_cilinder = "1560"
-var_co2 = "123"
-var_euro = "euro 5"
+var_co2 = "145"
+var_euro = "euro 4"
 var_options = """Alarm
-Bluetooth
 Elektrische buitenspiegels
 Climate control
-Mistlampen
 Radio
 Traction-control
 Verwarmde buitenspiegels
 ABS
 Airbags
 Boordcomputer
-Schuifdeur
 Centrale vergrendeling
-USB
 Airconditioning
 Elektrische ramen
 Electronic Stability Program (ESP)
 Isofix
 Metaalkleur
-Navigatiesysteem
 Parkeersensor
 Trekhaak
 Cruise Control"""

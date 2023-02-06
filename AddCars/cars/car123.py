d81918917cfadd2bef6c6914b308e6a4e9c06b63
Filip Luchianov <filip.luchianov@gmail.com>
2023-02-06 10:50:04 +0200

    dfhfg

--- AddCars/cars/car123.py
+++ AddCars/cars/car123.py
@@ -5,39 +5,24 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "citroen berlingo 1.6 benzine,46000 km,CAR PASS,garantie"
+var_title = "citroen berlingo XTR 1.6 hdi 68 kw CAR PASS,garantie,Euro 5"
 var_categorie = "Auto's"
 var_brand = "Citroën"
 var_model = "Berlingo"
-var_desc = "\n1 jaar garantie\nCAR PASS\nwordt gekeurd met roos formulier\n66 kw\n1587 cc benzine\neuro 4\n"
-var_price = "9860"
+var_desc = "\n1 jaar garantie\nCAR PASS\nwordt gekeurd met roos formulier\neerste eigenaar met oh-boekje\n68 kw\n"
+var_price = "7990"
 var_picspath = "car123"
 var_carroserie = "Monovolume"
 var_month = ""
-var_year = "2010"
-var_gas = "Benzine"
+var_year = "2012"
+var_gas = "Diesel"
 var_transmissie = "Hand"
-var_km = "46000"
+var_km = "141000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1587"
-var_co2 = ""
+var_cilinder = "1600"
+var_co2 = "139"
 var_euro = "Euro 4"
-var_options = """Alarm
-Elektrische buitenspiegels
-Climate control
-Mistlampen
-Radio
-Traction-control
-ABS
-Airbags
-Boordcomputer
-Schuifdeur
-Centrale vergrendeling
-Airconditioning
-Elektrische ramen
-Electronic Stability Program (ESP)
-Isofix
-Metaalkleur"""
+var_options = """ABS,Airbags,Airconditioning,Alarm,Boordcomputer,Centrale vergrendeling,Climate control,Electronic Stability Program (ESP),Elektrische buitenspiegels,Elektrische ramen,Isofix,Metaalkleur,Mistlampen,Radio,Schuifdeur,Traction-control,Trekhaak,Verwarmde buitenspiegels"""
 var_carcolor = ""
 var_interiorcolor = ""

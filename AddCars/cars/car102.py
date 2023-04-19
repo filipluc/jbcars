056741d9ba05cf58037541b729594ebf47a9576a
Filip Luchianov <filip.luchianov@gmail.com>
2023-04-19 08:51:48 +0300

    gfg

--- AddCars/cars/car102.py
+++ AddCars/cars/car102.py
@@ -5,41 +5,25 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "citroen c4 picasso 1.6 hdi,GEKEURD,7 zit,garantie,CAR PASS"
+var_title = "toyota verso-s 1.3 i,CAR PASS,77000 km,Bwj 2014,Garantie"
 var_categorie = "Auto's"
-var_brand = "Citroën"
-var_model = "C4"
-var_desc = "\n1 jaar garantie\nCAR PASS\ngekeurd met roos formulier\n80 kw\n7 zit\n144000 km\nnieuwe distributieriem+waterpomp\n"
-var_price = "5995"
+var_brand = "Toyota"
+var_model = "Verso-S"
+var_desc = "\n1 jaar garantie\nCAR PASS\nwordt gekeurd met roos formulier\neerste eigenaar met oh-boekje bij toyota\n1329 cc/73 kw\neuro 5\n127 co2\n"
+var_price = "9860"
 var_picspath = "car102"
 var_carroserie = "Monovolume"
 var_month = ""
-var_year = "2009"
-var_gas = "Diesel"
+var_year = "2014"
+var_gas = "Benzine"
 var_transmissie = "Hand"
-var_km = "144000"
+var_km = "77000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1560"
-var_co2 = "145"
-var_euro = "euro 4"
-var_options = """Alarm
-Elektrische buitenspiegels
-Climate control
-Radio
-Traction-control
-Verwarmde buitenspiegels
-ABS
-Airbags
-Boordcomputer
-Centrale vergrendeling
-Airconditioning
-Elektrische ramen
-Electronic Stability Program (ESP)
-Isofix
-Metaalkleur
-Parkeersensor
-Trekhaak
-Cruise Control"""
+var_cilinder = "1300"
+var_co2 = "127"
+var_euro = "euro 5"
+var_options = """
+ABS,Airbags,Airconditioning,Alarm,Bluetooth,Boordcomputer,Centrale vergrendeling,Climate control,Cruise Control,Electronic Stability Program (ESP),Elektrische buitenspiegels,Elektrische ramen,Isofix,Keyless entry,Lichtmetalen velgen,Metaalkleur,Mistlampen,Parkeercamera,Radio,Start-stop-systeem,Traction-control,Trekhaak,USB,Verwarmde buitenspiegels"""
 var_carcolor = ""
 var_interiorcolor = ""

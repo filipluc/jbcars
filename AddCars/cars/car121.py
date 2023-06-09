e3eb3535412e6558c1a4fb22a02c2ec16b9287db
Filip Luchianov <filip.luchianov@gmail.com>
2023-06-09 08:20:22 +0300

    gr

--- AddCars/cars/car121.py
+++ AddCars/cars/car121.py
@@ -5,46 +5,24 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "vw golf 1.6 d,bwj 2019,30000 km,Euro 6d,€ 16487+btw/tva"
+var_title = "peugeot partner 1.6 i,CAR PASS,€9916 excl. btw/tva,Euro 6b"
 var_categorie = "Auto's"
-var_brand = "Volkswagen"
-var_model = "Golf"
-var_desc = "\nprijs € 16487+btw/tva\n1 jaar garantie\nCAR PASS\nwordtgekeurd met roos formulier\n"
-var_price = "19950"
+var_brand = "Peugeot"
+var_model = "Partner"
+var_desc = "\n€ 9916+btw/tva\n1 jaar garantie\nCAR PASS\nwordt gekeurd met roos formulier\neerste eigenaar met oh-boekje\n"
+var_price = "11998"
 var_picspath = "car121"
 var_carroserie = ""
 var_month = ""
-var_year = "2019"
-var_gas = "Diesel"
+var_year = "2016"
+var_gas = "Benzine"
 var_transmissie = "Hand"
-var_km = "30000"
+var_km = "131000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "1598"
-var_co2 = ""
+var_cilinder = "1600"
+var_co2 = "148"
 var_euro = "euro 6"
-var_options = """Adaptive Cruise Control
-Alarm
-Bluetooth
-Parkeercamera
-Elektrische buitenspiegels
-Climate control
-Mistlampen
-Radio
-Traction-control
-Verwarmde buitenspiegels
-ABS
-Airbags
-Boordcomputer
-Centrale vergrendeling
-Airconditioning
-Elektrische ramen
-Electronic Stability Program (ESP)
-Isofix
-Lichtmetalen velgen
-Metaalkleur
-Navigatiesysteem
-Parkeersensor
-Cruise Control"""
+var_options = """ABS,Airbags,Airconditioning,Alarm,Boordcomputer,Centrale vergrendeling,Cruise Control,Electronic Stability Program (ESP),Elektrische ramen,Isofix,Metaalkleur,Radio,Schuifdeur,Traction-control,Verwarmde buitenspiegels"""
 var_carcolor = ""
 var_interiorcolor = ""

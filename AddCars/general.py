004035abea04473de6ad34a196a16cb410220f1d
Filip Luchianov <filip.luchianov@gmail.com>
2023-08-18 12:09:55 +0300

    delete update

--- AddCars/general.py
+++ AddCars/general.py
@@ -82,6 +82,13 @@ class GeneralFunctions():
         elem103 = driver.find_element_by_xpath("//button[contains(text(), 'Verkocht via 2dehands')]")
         time.sleep(1)
         elem103.click()
+        time.sleep(1)
+        try:
+            elem104 = driver.find_element_by_xpath("//button[text() = 'Direct']")
+            elem104.click()
+            time.sleep(1)
+        except NoSuchElementException:
+            pass
         time.sleep(2)
 
     

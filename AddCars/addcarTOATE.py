f770b864322e85ae01a989f1bde7672d8d00ad05
Filip Luchianov <filip.luchianov@gmail.com>
2023-11-14 17:11:02 +0200

    dfg

--- AddCars/addcarTOATE.py
+++ AddCars/addcarTOATE.py
@@ -122,8 +122,8 @@ class AdaugaMasini(unittest.TestCase):
     def test_car111(self):
         GeneralFunctions.deleteAddCarFunction(self, car111)
 
-    def test_car112(self):
-        GeneralFunctions.deleteAddCarFunction(self, car112)
+    # def test_car112(self):
+    #     GeneralFunctions.deleteAddCarFunction(self, car112)
 
     def test_car113(self):
         GeneralFunctions.deleteAddCarFunction(self, car113)
@@ -133,9 +133,9 @@ class AdaugaMasini(unittest.TestCase):
         
     def test_car115(self):
         GeneralFunctions.deleteAddCarFunction(self, car115)
-
-    def test_car116(self):
-        GeneralFunctions.deleteAddCarFunction(self, car116)
+    #
+    # def test_car116(self):
+    #     GeneralFunctions.deleteAddCarFunction(self, car116)
 
     def test_car117(self):
         GeneralFunctions.deleteAddCarFunction(self, car117)
@@ -143,8 +143,8 @@ class AdaugaMasini(unittest.TestCase):
     # def test_car118(self):
     #     GeneralFunctions.deleteAddCarFunction(self, car118)
         
-    def test_car119(self):
-        GeneralFunctions.deleteAddCarFunction(self, car119)
+    # def test_car119(self):
+    #     GeneralFunctions.deleteAddCarFunction(self, car119)
 
     # def test_car120(self):
     #     GeneralFunctions.deleteAddCarFunction(self, car120)

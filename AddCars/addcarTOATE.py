2cd65b23f170b932c069e679d81ba550ea4f7329
Filip Luchianov <filip.luchianov@gmail.com>
2024-02-17 10:59:46 +0200

    fdg

--- AddCars/addcarTOATE.py
+++ AddCars/addcarTOATE.py
@@ -152,9 +152,9 @@ class AdaugaMasini(unittest.TestCase):
     def test_car121(self):
         GeneralFunctions.deleteAddCarFunction(self, car121)
 
-    # def test_car122(self):
-    #     GeneralFunctions.deleteAddCarFunction(self, car122)
-    #
+    def test_car122(self):
+        GeneralFunctions.deleteAddCarFunction(self, car122)
+
     # def test_car123(self):
     #     GeneralFunctions.deleteAddCarFunction(self, car123)
 

98f30ea0daaf447bb1deea2a46baf223027a312f
Filip Luchianov <filip.luchianov@gmail.com>
2024-03-25 15:03:06 +0200

    dfg

--- AddCars/cars/car101.py
+++ AddCars/cars/car101.py
@@ -5,55 +5,24 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "jeep grand cherokee 3.0 crd ,START NIET, export,Demarre pas"
+var_title = "ford tourneo courier 1.5 tdci,CAR PASS,bwj 2016,euro 5b"
 var_categorie = "Auto's"
-var_brand = "Jeep"
-var_model = "Cherokee"
-var_desc = "\nSTART NIET/demarre pas !!!\nexport\n"
-var_price = "6000"
+var_brand = "Ford"
+var_model = "Tourneo"
+var_desc = "\nzo meenemen prijs\nauto rijdt goed\nCAR PASS\nbwj 02/2016\n55 kw\n1499 cc\n108 co2\n"
+var_price = "7150"
 var_picspath = "car101"
-var_carroserie = "SUV"
+var_carroserie = "Monovolume"
 var_month = ""
-var_year = "2012"
+var_year = "2016"
 var_gas = "Diesel"
-var_transmissie = "Auto"
-var_km = "220000"
+var_transmissie = "Hand"
+var_km = "200000"
 var_doors = ""
 var_pk = ""
-var_cilinder = "2987"
-var_co2 = ""
+var_cilinder = "1500"
+var_co2 = "108"
 var_euro = "euro 5"
-var_options = """Stoelventilatie
-Keyless entry
-4x4
-Alarm
-Bluetooth
-Parkeercamera
-Elektrische buitenspiegels
-Elektrische stoelverstelling
-Climate control
-Mistlampen
-Radio
-Start-stop-systeem
-Traction-control
-Verwarmde buitenspiegels
-Xenon verlichting
-Elektrische koffer
-ABS
-Airbags
-Boordcomputer
-Centrale vergrendeling
-Airconditioning
-Panoramadak
-Elektrische ramen
-Electronic Stability Program (ESP)
-Isofix
-Lichtmetalen velgen
-Metaalkleur
-Navigatiesysteem
-Open dak
-Parkeersensor
-Zetelverwarming
-Cruise Control"""
-var_carcolor = "Zwart"
+var_options = """ABS,Airbags,Airconditioning,Alarm,Bluetooth,Boordcomputer,Centrale vergrendeling,Cruise Control,Dakrails,Electronic Stability Program (ESP),Elektrische buitenspiegels,Elektrische ramen,Isofix,Lichtmetalen velgen,Lichtsensor,Metaalkleur,Mistlampen,Parkeersensor,Radio,Regensensor,Schuifdeur,Traction-control,Trekhaak,USB,Verwarmde buitenspiegels"""
+var_carcolor = "Overige kleuren"
 var_interiorcolor = ""

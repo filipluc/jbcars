9ff81d150af0a759b3ec42d2251d19fddba7e29d
Filip Luchianov <filip.luchianov@gmail.com>
2024-04-07 15:57:49 +0300

    red

--- AddCars/cars/car117.py
+++ AddCars/cars/car117.py
@@ -8,7 +8,7 @@ Created on Jun 3, 2018
 var_title = "fiat 500, 1.2 i,36000 km,CAR PASS,euro 6b,Garantie 1 jaar"
 var_categorie = "Auto's"
 var_brand = "Fiat"
-var_model = "500"
+var_model = "5555555"
 var_desc = "\n1 jaar garantie\nCAR PASS\nwordt gekeurd voor verkoop met roos formulier\neerste eigenaar\nbwj 07/2015\n36000 km\n51 kw\n115 co2\neuro 6b\n1242 cc\n"
 var_price = "6990"
 var_picspath = "car117"

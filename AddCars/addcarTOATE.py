30f8b71af8e86db6c668b2100ff5f95106c448d1
Filip Luchianov <filip.luchianov@gmail.com>
2024-10-09 13:29:04 +0300

    ffgfg

--- AddCars/addcarTOATE.py
+++ AddCars/addcarTOATE.py
@@ -194,8 +194,8 @@ class AdaugaMasini(unittest.TestCase):
     def test_car128(self):
         GeneralFunctions.deleteAddCarFunction(self, car128)
 
-    # def test_car129(self):
-    #     GeneralFunctions.deleteAddCarFunction(self, car129)
+    def test_car129(self):
+        GeneralFunctions.deleteAddCarFunction(self, car129)
 
     # def test_car130(self):
     #     GeneralFunctions.deleteAddCarFunction(self, car130)
--- AddCars/cars/car125.py
+++ AddCars/cars/car125.py
@@ -5,50 +5,24 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "mercedes GLC coupe 250, 4-Matic"
+var_title = "nissan micra 1.2 i, AUTOMAAT,euro 6b,CAR PASS,garantie"
 var_categorie = "Auto's"
-var_brand = "Mercedes-Benz"
-var_model = "GLC-klasse"
-var_desc = "\nCAR PASS\ngekeurd met roos formulier\n\n"
-var_price = "44900"
+var_brand = "Nissan"
+var_model = "Micra"
+var_desc = "\nAUTOMAAT\n\n1 jaar garantie\nCAR PASS\nwordt gekeurd voor verkoop\neerste eigenaar met oh-boekje bij dealer\neuro 6b\nbwj 08/2017\n44900 km\n1198 cc benzine\n125 co2\n59 kw\n"
+var_price = "10690"
 var_picspath = "car125"
-var_carroserie = "SUV"
+var_carroserie = "Stadsauto"
 var_month = ""
 var_year = "2017"
-var_gas = "Diesel"
+var_gas = "Benzine"
 var_transmissie = "Auto"
-var_km = "139000"
+var_km = "44900"
 var_doors = ""
 var_pk = ""
-var_cilinder = "2143"
-var_co2 = ""
+var_cilinder = "1200"
+var_co2 = "125"
 var_euro = "euro 6"
-var_options = """Cruise Control
-Startonderbreker
-Bluetooth
-Parkeercamera
-Navigatiesysteem
-Elektrische buitenspiegels
-Elektrische stoelverstelling
-Parkeersensor
-Elektrische ramen
-4x4
-Adaptieve lichten
-Alarm
-Lichtmetalen velgen
-Metaalkleur
-Boordcomputer
-Centrale vergrendeling
-Airconditioning
-Start-stop-systeem
-Traction-control
-Verwarmde buitenspiegels
-Xenon verlichting
-ABS
-Mistlampen
-Airbags
-Zetelverwarming
-Radio
-Sportpakket"""
-var_carcolor = "Zwart"
+var_options = """ABS,Airbags,Airconditioning,Alarm,Bluetooth,Boordcomputer,Centrale vergrendeling,Climate control,Cruise Control,Electronic Stability Program (ESP),Elektrische buitenspiegels,Elektrische ramen,Isofix,Lichtmetalen velgen,Metaalkleur,Mistlampen,Navigatiesysteem,Parkeersensor,Radio,Regensensor,Traction-control,Verwarmde buitenspiegels"""
+var_carcolor = "Bruin"
 var_interiorcolor = ""

5dc9fc76ee74e017fb6dab9b6d10a2c7e4a2f6c1
Filip Luchianov <filip.luchianov@gmail.com>
2025-08-01 11:31:03 +0300

    dfgd

--- AddCars/cars/car118.py
+++ AddCars/cars/car118.py
@@ -5,12 +5,12 @@ Created on Jun 3, 2018
 '''
 
 
-var_title = "hyundai sonata  2.0 crdi ,AUTOMAAT, CAR PASS"
+var_title = "hyundai sonata 2.0 diesel/AUTOMAAT/CAR PASS/garantie"
 var_categorie = "Auto's"
 var_brand = "Hyundai"
 var_model = "Sonata"
-var_desc = "\nAUTOMAAT\nCAR PASS\nzo meenemen\nbwj 2010\n1991 cc\n184000 km\n110 kw\n184 co2\neuro 4\nfull option\n"
-var_price = "5000"
+var_desc = "\nAUTOMAAT\nCAR PASS\nwordt gekeurd voor verkoop\n1 jaar garantie\nbwj 2010\n1991 cc diesel\n184000 km\n110 kw\n184 co2\neuro 4\n"
+var_price = "5490"
 var_picspath = "car118"
 var_carroserie = "Berline"
 var_month = ""
@@ -23,6 +23,6 @@ var_pk = ""
 var_cilinder = "2000"
 var_co2 = "184"
 var_euro = "euro 4"
-var_options = """ABS,Achteruitrijcamera,Airbags,Airconditioning,Alarm,Boordcomputer,Centrale vergrendeling,Climate control,Cruise Control,Electronic Stability Program (ESP),Elektrische buitenspiegels,Elektrische ramen,Isofix,Lederen bekleding,Lichtmetalen velgen,Lichtsensor,Metaalkleur,Mistlampen,Navigatiesysteem,Parkeercamera,Parkeersensor,Radio,Regensensor,Traction-control,Trekhaak,Verwarmde buitenspiegels"""
+var_options = """ABS,Airbags,Airconditioning,Alarm,Bluetooth,Boordcomputer,Centrale vergrendeling,Climate control,Cruise Control,Electronic Stability Program (ESP),Elektrische buitenspiegels,Elektrische ramen,Isofix,Lichtmetalen velgen,Metaalkleur,Mistlampen,Navigatiesysteem,Parkeersensor,Radio,Regensensor,Traction-control,Trekhaak,USB,Verwarmde buitenspiegels,Zetelverwarming"""
 var_carcolor = "Zwart"
 var_interiorcolor = ""
